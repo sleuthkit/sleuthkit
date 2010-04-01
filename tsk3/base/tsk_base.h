ac9b088c32a28adefeda0f669986afbec3cbdaf4
Brian Carrier <carrier@sleuthkit.org>
2010-04-01 01:44:38 +0000

    New version files for 3.1.1

--- tsk3/base/tsk_base.h
+++ tsk3/base/tsk_base.h
@@ -38,11 +38,11 @@
  * 3.1.2b1 would be 0x03010201.  Snapshot from Jan 2, 2003 would be
  * 0xFF030102. 
  * See TSK_VERSION_STR for string form. */ 
-#define TSK_VERSION_NUM 0x03010000  
+#define TSK_VERSION_NUM 0x030101ff
 
 /** Version of code in string form. See TSK_VERSION_NUM for 
  * integer form. */
-#define TSK_VERSION_STR "3.1.0"
+#define TSK_VERSION_STR "3.1.1"
 
 
 /* include the TSK-specific header file that we created in autoconf
