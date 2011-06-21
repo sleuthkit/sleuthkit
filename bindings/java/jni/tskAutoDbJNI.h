5bea5a94a4975cb10c59e0f0adb934876b22d6fb
Brian Carrier <carrier@sleuthkit.org>
2011-06-21 20:10:37 +0000

    removed duplicate code and fixed jni proj

--- bindings/java/jni/tskAutoDbJNI.h
+++ bindings/java/jni/tskAutoDbJNI.h
@@ -6,11 +6,3 @@ class TskAutoDbJNI:public TskAutoDb {
         const char *path);
       void cancelProcess();
 };
-class TskAutoDbJNI:public TskAutoDb {
-  public:
-      bool m_cancelled;
-      TskAutoDbJNI();
-      virtual TSK_RETVAL_ENUM processFile(TSK_FS_FILE * fs_file,
-        const char *path);
-      void cancelProcess();
-};
