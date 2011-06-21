2abfc6872db118bde62f670a9120d0f51ebbcda0
Brian Carrier <carrier@sleuthkit.org>
2011-06-21 20:09:26 +0000

    removed duplicate entries

--- bindings/java/src/org/sleuthkit/datamodel/TskException.java
+++ bindings/java/src/org/sleuthkit/datamodel/TskException.java
@@ -16,20 +16,3 @@ public class TskException extends Exception{
 
 }
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package org.sleuthkit.datamodel;
-
-/**
- *
- * @author alawrence
- */
-public class TskException extends Exception{
-    public TskException(String msg){
-        super(msg);
-    }
-
-}
