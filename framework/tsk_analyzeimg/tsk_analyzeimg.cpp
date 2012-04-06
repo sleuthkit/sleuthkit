0c86b11a9845bb137d4f8f99c75ed89ecf0723b6
Joe Ho <jkho@basistech.com>
2012-04-06 08:30:53 -0700

    MAY-324. Add optional -d outdir parameter. Update README.txt. Use @OUT in pipeline_config.xml.

--- framework/tsk_analyzeimg/tsk_analyzeimg.cpp
+++ framework/tsk_analyzeimg/tsk_analyzeimg.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <string>
 
 #include "tsk3/tsk_tools_i.h"
 #include "framework.h"
@@ -37,9 +38,10 @@ makeDir(const TSK_TCHAR *dir)
 void 
 usage(const char *program) 
 {
-    fprintf(stderr, "%s [-c framework_config_file] [-p pipeline_config_file] image_name\n", program);
+    fprintf(stderr, "%s [-c framework_config_file] [-p pipeline_config_file] [-d outdir] image_name\n", program);
     fprintf(stderr, "\t-c framework_config_file: Path to XML framework config file\n");
     fprintf(stderr, "\t-p pipeline_config_file: Path to XML pipeline config file (overrides pipeline config specified with -c)\n");
+    fprintf(stderr, "\t-d outdir: Path to output directory\n");
     exit(1);
 }
 
@@ -51,6 +53,7 @@ int main(int argc, char **argv1)
     struct STAT_STR stat_buf;
     TSK_TCHAR *pipeline_config = NULL;
     TSK_TCHAR *framework_config = NULL;
+    std::wstring outDirPath;
 
 #ifdef TSK_WIN32
     // On Windows, get the wide arguments (mingw doesn't support wmain)
@@ -64,8 +67,8 @@ int main(int argc, char **argv1)
 #endif
 
     while ((ch =
-        GETOPT(argc, argv, _TSK_T("c:p:vV"))) > 0) {
-            switch (ch) {
+        GETOPT(argc, argv, _TSK_T("d:c:p:vV"))) > 0) {
+        switch (ch) {
         case _TSK_T('?'):
         default:
             TFPRINTF(stderr, _TSK_T("Invalid argument: %s\n"),
@@ -83,7 +86,11 @@ int main(int argc, char **argv1)
         case _TSK_T('V'):
             tsk_version_print(stdout);
             exit(0);
-            }
+            break;
+        case _TSK_T('d'):
+            outDirPath.assign(OPTARG);
+            break;
+        }
     }
 
     /* We need at least one more argument */
@@ -91,6 +98,7 @@ int main(int argc, char **argv1)
         tsk_fprintf(stderr, "Missing image name\n");
         usage(argv1[0]);
     }
+
     TSK_TCHAR *imagePath = argv[OPTIND];
 
     // Load the framework config if they specified it
@@ -110,15 +118,16 @@ int main(int argc, char **argv1)
         TskServices::Instance().setSystemProperties(*systemProperties);
     }
 
-    // make up an output folder to store the database and such in
-    TSK_TCHAR outDirPath[1024];
-    TSNPRINTF(outDirPath, 1024, _TSK_T("%s_tsk_out"), imagePath);
-    if (TSTAT(outDirPath, &stat_buf) == 0) {
-        fprintf(stderr, "Output directory already exists (%"PRIttocTSK")\n", outDirPath);
+    if (outDirPath == _TSK_T("")) {
+        outDirPath.assign(imagePath);
+        outDirPath.append(_TSK_T("_tsk_out"));
+    }
+    if (TSTAT(outDirPath.c_str(), &stat_buf) == 0) {
+        fprintf(stderr, "Output directory already exists (%"PRIttocTSK")\n", outDirPath.c_str());
         return 1;
     }
 
-    if (makeDir(outDirPath)) {
+    if (makeDir(outDirPath.c_str())) {
         return 1;
     }
 
@@ -127,7 +136,7 @@ int main(int argc, char **argv1)
 
     // Create and register our SQLite ImgDB class   
     std::auto_ptr<TskImgDB> pImgDB(NULL);
-    pImgDB = std::auto_ptr<TskImgDB>(new TskImgDBSqlite(outDirPath));
+    pImgDB = std::auto_ptr<TskImgDB>(new TskImgDBSqlite(outDirPath.c_str()));
     if (pImgDB->initialize() != 0) {
         fprintf(stderr, "Error initializing SQLite database\n");
         tsk_error_print(stderr);
