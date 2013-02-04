9fd1a77f57b73c1135d3c9a326614c3d30aeea0c
esaunders <esaunders@basistech.com>
2013-02-04 12:01:26 -0500

    Added new Pipeline API that takes an enum for the pipeline type and accepts an optional name. This introduces support for multiple pipelines of the same type (with different names).

--- framework/Pipeline/TskPipelineManager.cpp
+++ framework/Pipeline/TskPipelineManager.cpp
@@ -37,11 +37,12 @@
 #include "Poco/SAX/InputSource.h"
 #include "Poco/SAX/SAXException.h"
 
-const std::string TskPipelineManager::FILE_ANALYSIS_PIPELINE = "FileAnalysis";
-const std::string TskPipelineManager::REPORTING_PIPELINE = "Report";
-const std::string TskPipelineManager::POST_PROCESSING_PIPELINE = "PostProcessing";
+const std::string TskPipelineManager::FILE_ANALYSIS_PIPELINE_STR = "FileAnalysis";
+const std::string TskPipelineManager::POST_PROCESSING_PIPELINE_STR = "PostProcessing";
+const std::string TskPipelineManager::REPORTING_PIPELINE_STR = "Report";
 const std::string TskPipelineManager::PIPELINE_ELEMENT = "PIPELINE";
-const std::string TskPipelineManager::PIPELINE_TYPE = "type";
+const std::string TskPipelineManager::PIPELINE_TYPE_ATTRIBUTE = "type";
+const std::string TskPipelineManager::PIPELINE_NAME_ATTRIBUTE = "name";
 
 TskPipelineManager::TskPipelineManager()
 {
@@ -54,29 +55,24 @@ TskPipelineManager::~TskPipelineManager()
         delete *it;
 }
 
-/**
- * Creates a pipeline object by reading the pipeline config file specified as
- * a system property. 
- * @returns Pointer to TskPipeline.  Do not free this. It will be freed by the
- * TskPipelineManager destructor. 
- */
-TskPipeline * TskPipelineManager::createPipeline(const std::string &pipelineType)
+TskPipeline * TskPipelineManager::createPipeline(const PIPELINE_TYPE type, const std::string& name)
 {
+    std::string funcName("TskPipelineManager::createPipeline");
+
     std::string pipelineConfigFilePath = GetSystemProperty(TskSystemProperties::PIPELINE_CONFIG_FILE);
     std::ifstream in(pipelineConfigFilePath.c_str());
     if (!in)
     {
-        std::stringstream errorMsg;
-        errorMsg << "TskPipelineManager::createPipeline : error opening config file '" << pipelineConfigFilePath << "' to create " << pipelineType << " pipeline";
-        LOGERROR(errorMsg.str());
         throw TskException("Error opening pipeline config file.");
     }
     else 
     {
         std::stringstream msg;
-        msg << "TskPipelineManager::createPipeline : using config file '" << pipelineConfigFilePath << "' to create " << pipelineType << " pipeline";
+        msg << funcName << " : Using config file '" << pipelineConfigFilePath;
         LOGINFO(msg.str());
     }
+
+    std::string pipelineType = pipelineTypeToString(type);
 
     try
     {
@@ -92,15 +88,13 @@ TskPipeline * TskPipelineManager::createPipeline(const std::string &pipelineType
 
         if (pipelines->length() == 0)
         {
-            LOGERROR(L"TskPipelineManager::createPipeline - No pipelines found in config file.");
-
             throw TskException("No pipelines found in config file.");
         }
 
         TskPipeline * pipeline;
-        if (pipelineType == FILE_ANALYSIS_PIPELINE)
+        if (type == FILE_ANALYSIS_PIPELINE)
             pipeline = new TskFileAnalysisPipeline();
-        else if (pipelineType == REPORTING_PIPELINE || pipelineType == POST_PROCESSING_PIPELINE)
+        else if (type == POST_PROCESSING_PIPELINE)
             pipeline = new TskReportPipeline();
         else
             throw TskException("Unsupported pipeline type.");
@@ -114,24 +108,30 @@ TskPipeline * TskPipelineManager::createPipeline(const std::string &pipelineType
 
             if (pElem)
             {
-                std::string xmlPipelineType = pElem->getAttribute(TskPipelineManager::PIPELINE_TYPE);
+                std::string xmlPipelineType = pElem->getAttribute(TskPipelineManager::PIPELINE_TYPE_ATTRIBUTE);
+                std::string xmlPipelineName = pElem->getAttribute(TskPipelineManager::PIPELINE_NAME_ATTRIBUTE);
 
                 // The following conditions are required because we want to be able to use 
                 // "PostProcessing" and "Report" to be used interchangeably (at least for the moment).
                 // Note that the sanity check below will not catch the case where there are both
                 // "PostProcessing" and "Report" pipelines in the configuration file.
-                if ((xmlPipelineType == pipelineType) ||
-                    (pipelineType == REPORTING_PIPELINE && xmlPipelineType == POST_PROCESSING_PIPELINE) ||
-                    (pipelineType == POST_PROCESSING_PIPELINE && xmlPipelineType == REPORTING_PIPELINE))
+                if (xmlPipelineName == name && (xmlPipelineType == pipelineType) ||
+                    (pipelineType == REPORTING_PIPELINE_STR && xmlPipelineType == POST_PROCESSING_PIPELINE_STR) ||
+                    (pipelineType == POST_PROCESSING_PIPELINE_STR && xmlPipelineType == REPORTING_PIPELINE_STR))
                 {
-                    // quick sanity check to verify that there is only one pipeline in the config file for this type
+                    // quick sanity check to verify that there is only one pipeline in the config file for this type and name
                     for (unsigned long i2 = i+1; i2 < pipelines->length(); i2++) {
                         Poco::XML::Node * pNode2 = pipelines->item(i2);
                         Poco::XML::Element* pElem2 = dynamic_cast<Poco::XML::Element*>(pNode2);
 
-                        if (pElem2 && pElem2->getAttribute(TskPipelineManager::PIPELINE_TYPE) == pipelineType) {
-                            LOGERROR(L"TskPipelineManager::createPipeline: Multiple pipelines of the same type exist");
-                            throw TskException ("Error creating pipeline");
+                        if (pElem2 && 
+                            pElem2->getAttribute(TskPipelineManager::PIPELINE_TYPE_ATTRIBUTE) == pipelineType &&
+                            pElem2->getAttribute(TskPipelineManager::PIPELINE_NAME_ATTRIBUTE) == name) 
+                        {
+                            std::stringstream exMsg;
+                            exMsg << "Multiple pipelines found with the same type (" << pipelineType
+                                << ") and name (" << name << ")";
+                            throw TskException (exMsg.str());
                         }
                     }
                     // We found the right pipeline so initialize it.
@@ -148,23 +148,50 @@ TskPipeline * TskPipelineManager::createPipeline(const std::string &pipelineType
     }
     catch (Poco::XML::SAXParseException& )
     {
-        LOGERROR(L"TskPipelineManager::createPipeline - Error parsing pipeline config file.");
         throw TskException("Error parsing pipeline config file.");
     }
     catch (TskException& ex)
     {
-        std::wstringstream errorMsg;
-        errorMsg << L"TskPipelineManager::createPipeline - Error creating pipeline: "
-            << ex.message().c_str() ;
-        LOGERROR(errorMsg.str());
-
-        throw TskException("Error creating pipeline.");
+        throw ex;
     }
 
-    std::wstringstream errorMsg;
-    errorMsg << L"TskPipelineManager::createPipeline - Failed to find pipeline for "
-        << pipelineType.c_str() ;
-    LOGERROR(errorMsg.str());
+    std::stringstream errorMsg;
+    errorMsg << "Failed to find " << pipelineType << " pipeline";
+    if (!name.empty())
+        errorMsg << " with name " << name;
 
-    throw TskException("Failed to find pipeline for " + pipelineType);
+    throw TskException(errorMsg.str());
+}
+
+/**
+ * Creates a pipeline object by reading the pipeline config file specified as
+ * a system property. 
+ * @returns Pointer to TskPipeline.  Do not free this. It will be freed by the
+ * TskPipelineManager destructor. 
+ */
+TskPipeline * TskPipelineManager::createPipeline(const std::string &pipelineType)
+{
+    if (pipelineType == REPORTING_PIPELINE_STR || pipelineType == POST_PROCESSING_PIPELINE_STR)
+        return createPipeline(TskPipelineManager::POST_PROCESSING_PIPELINE);
+    else if (pipelineType == FILE_ANALYSIS_PIPELINE_STR)
+        return createPipeline(TskPipelineManager::FILE_ANALYSIS_PIPELINE);
+    else
+    {
+        std::stringstream exMsg;
+        exMsg << "TskPipelineManager::createPipeline : Unsupported pipeline type : " << pipelineType;
+        throw TskException(exMsg.str());
+    }
+}
+
+std::string TskPipelineManager::pipelineTypeToString(const PIPELINE_TYPE type)
+{
+    switch (type)
+    {
+    case FILE_ANALYSIS_PIPELINE:
+        return FILE_ANALYSIS_PIPELINE_STR;
+    case POST_PROCESSING_PIPELINE:
+        return POST_PROCESSING_PIPELINE_STR;
+    default:
+        return "Unknown pipeline type";
+    }
 }
