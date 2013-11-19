d73b42c9f9128c1364e48c8bc9f35a7ae46b1c36
Jeff Wallace <jwallace@basistech.com>
2013-11-19 15:09:37 -0500

    Uncommented img info code in c++ db code.

--- tsk/auto/db_sqlite.cpp
+++ tsk/auto/db_sqlite.cpp
@@ -237,9 +237,7 @@ int
             "Error creating tsk_objects table: %s\n")
         ||
         attempt_exec
-//        ("CREATE TABLE tsk_image_info (obj_id INTEGER PRIMARY KEY, type INTEGER, ssize INTEGER, tzone TEXT, size INTEGER, md5 TEXT);",
-
-        ("CREATE TABLE tsk_image_info (obj_id INTEGER PRIMARY KEY, type INTEGER, ssize INTEGER, tzone TEXT);",
+        ("CREATE TABLE tsk_image_info (obj_id INTEGER PRIMARY KEY, type INTEGER, ssize INTEGER, tzone TEXT, size INTEGER, md5 TEXT);",
             "Error creating tsk_image_info table: %s\n")
         ||
         attempt_exec
@@ -434,13 +432,9 @@ int
 
     objId = sqlite3_last_insert_rowid(m_db);
 
-//    snprintf(stmt, 1024,
-//        "INSERT INTO tsk_image_info (obj_id, type, ssize, tzone, size, md5) VALUES (%lld, %d, %d, '%s', %"PRIuOFF", '%s');",
-//        objId, type, ssize, timezone.c_str(), size, md5.c_str());
-    
     snprintf(stmt, 1024,
-        "INSERT INTO tsk_image_info (obj_id, type, ssize, tzone) VALUES (%lld, %d, %d, '%s');",
-        objId, type, ssize, timezone.c_str());
+        "INSERT INTO tsk_image_info (obj_id, type, ssize, tzone, size, md5) VALUES (%lld, %d, %d, '%s', %"PRIuOFF", '%s');",
+        objId, type, ssize, timezone.c_str(), size, md5.c_str());
 
     return attempt_exec(stmt,
         "Error adding data to tsk_image_info table: %s\n");
