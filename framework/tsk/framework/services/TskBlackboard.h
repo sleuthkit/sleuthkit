c82fa738613bfa4bd97ed20ef28af49943f8edb6
APriestman <apriestman@basistech.com>
2013-11-27 14:53:22 -0500

    Added TSK_PROG_RUN artifact

--- framework/tsk/framework/services/TskBlackboard.h
+++ framework/tsk/framework/services/TskBlackboard.h
@@ -77,7 +77,7 @@ enum TSK_ARTIFACT_TYPE {
 	TSK_GPS_BOOKMARK = 29, ///< GPS Bookmarks
 	TSK_GPS_LAST_KNOWN_LOCATION = 30, ///< GPS Last known location
 	TSK_GPS_SEARCH = 31,	///< GPS Searches
-		 
+	TSK_PROG_RUN = 32, ///< Application run information
 
 		
     /* SEE ABOVE:
@@ -193,6 +193,7 @@ enum TSK_ATTRIBUTE_TYPE {
 	TSK_PATH_SOURCE = 94, ///< Path to a source file related to the artifact
 	TSK_PERMISSIONS = 95, ///< Permissions
 	TSK_PROG_LAUNCHED = 96, ///< Flag for whether a program has been run
+	TSK_ASSOCIATED_ARTIFACT = 97, ///< Artifact ID of a related artifact
 
     /* SEE ABOVE: 
     * - KEEP JAVA CODE IN SYNC 
