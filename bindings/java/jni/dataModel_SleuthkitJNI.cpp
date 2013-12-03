01d5a792041459fc251325223e0cca674e294664
Samuel H. Kenyon <skenyon@basistech.com>
2013-12-03 15:50:37 -0500

    Add some more info to the hashDbCreateIndexNat error msg.

--- bindings/java/jni/dataModel_SleuthkitJNI.cpp
+++ bindings/java/jni/dataModel_SleuthkitJNI.cpp
@@ -1687,8 +1687,14 @@ Java_org_sleuthkit_datamodel_SleuthkitJNI_hashDbCreateIndexNat (JNIEnv * env,
   
         // [Re]create the hash information and file
         uint8_t err = tsk_hdb_regenerate_index(db, dbType, (overwrite ? 1 : 0));
+
+        // Make an error message
         if (err > 0) {
-            std::string msg("Error: index regeneration: ");
+            char c_db_type[32];
+            snprintf(c_db_type, 32, "%" PRIttocTSK, dbType);
+            std::string dbTypeStr(c_db_type);
+
+            std::string msg("Error: index regeneration (db_type = " + dbTypeStr + "): ");
             switch (err) {
                 case 1: 
                     msg += "delete old failed.";
@@ -1704,6 +1710,7 @@ Java_org_sleuthkit_datamodel_SleuthkitJNI_hashDbCreateIndexNat (JNIEnv * env,
             return;
         }
 
+        // success
         return;
     }
 }
