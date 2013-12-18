a9ac11bfdc4dea8ab43e08db178046cb5627b494
Richard Cordovano <rcordovano@basistech.com>
2013-12-18 13:50:27 -0500

    Remove obsolete, commented out code in JNI bindings

--- bindings/java/jni/dataModel_SleuthkitJNI.cpp
+++ bindings/java/jni/dataModel_SleuthkitJNI.cpp
@@ -1740,35 +1740,6 @@ JNIEXPORT jboolean JNICALL Java_org_sleuthkit_datamodel_SleuthkitJNI_hashDbIndex
     }
 }
 
-///*
-// * Get the size of the index for the database at the given path
-// * @param env pointer to java environment this was called from
-// * @param obj the java object this was called from
-// * @param dbPathJ the path for the database
-// * @return -1 on error, otherwise size of index
-// */
-//JNIEXPORT jint JNICALL Java_org_sleuthkit_datamodel_SleuthkitJNI_getIndexSizeNat
-//  (JNIEnv * env, jclass obj, jstring dbPathJ) {
-//
-//    TSK_TCHAR dbPathT[1024];
-//    toTCHAR(env, dbPathT, 1024, dbPathJ);
-//
-//    TSK_HDB_OPEN_ENUM flags = TSK_HDB_OPEN_IDXONLY;
-//    TSK_HDB_INFO * temp = tsk_hdb_open(dbPathT, flags);
-//    if (temp == NULL) {
-//        return -1;
-//    }
-//
-//    if(tsk_hdb_idxsetup(temp, TSK_HDB_HTYPE_MD5_ID)) {
-//        return (jint) ((temp->idx_info->idx_struct.idx_binsrch->idx_size - temp->idx_info->idx_struct.idx_binsrch->idx_off) / (temp->idx_info->idx_struct.idx_binsrch->idx_llen));
-//    }
-//
-//
-//    tsk_hdb_close(temp);
-//    return -1;
-//}
-
-
 /*
  * Query and get size of the device (such as physical disk, or image) pointed by the path
  * Might require elevated priviletes to work (otherwise will error)
