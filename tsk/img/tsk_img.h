89695380baf73428abc9d22de7ab212611c83a30
Brian Carrier <carrier@sleuthkit.org>
2014-03-08 22:49:51 -0500

    manual merge into develop branch from #306 from @joachimmetz to add external type to image layer

--- tsk/img/tsk_img.h
+++ tsk/img/tsk_img.h
@@ -65,6 +65,7 @@ extern "C" {
         TSK_IMG_TYPE_AFF_ANY = 0x0020,  ///< Any format supported by AFFLIB (including beta ones)
 
         TSK_IMG_TYPE_EWF_EWF = 0x0040,  ///< EWF version
+        TSK_IMG_TYPE_EXTERNAL = 0x1000,  ///< external defined format which at least implements TSK_IMG_INFO, used by pytsk
 
         TSK_IMG_TYPE_UNSUPP = 0xffff,   ///< Unsupported disk image type
     } TSK_IMG_TYPE_ENUM;
