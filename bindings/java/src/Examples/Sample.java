45d005bf1de3fc43cc43a8b31b476a19bcbf76c7
alexjacks92 <alexjacks@comcast.net>
2014-05-13 14:59:25 -0400

    First pass at sample program.

---
+++ bindings/java/src/Examples/Sample.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Examples;
+import java.util.List;
+import org.sleuthkit.datamodel.AbstractFile;
+import org.sleuthkit.datamodel.Content;
+import org.sleuthkit.datamodel.Image;
+import org.sleuthkit.datamodel.SleuthkitCase;
+import org.sleuthkit.datamodel.TskCoreException;
+/**
+ *
+ */
+public class Sample {
+	public static void run(String imagePath) {
+		try {
+		SleuthkitCase sk = SleuthkitCase.newCase(imagePath + ".db");
+		
+		// ADD IMAGE TO CASE??
+		
+		List<Image> images = sk.getImages();
+		
+		for (Image image : images) {
+			for (Content content : image.getChildren()) {
+				System.out.println(content.getName());
+			}
+		}
+		
+		List<AbstractFile> files = sk.findFiles(null, "*.txt");
+		
+		for (AbstractFile file : files) {
+			System.out.println(file.getLocalAbsPath());
+		}
+		
+		} catch (TskCoreException e) {
+			System.out.println("Exception caught: " + e.getMessage());
+		}
+		
+		
+		
+		
+				
+	}
+}
