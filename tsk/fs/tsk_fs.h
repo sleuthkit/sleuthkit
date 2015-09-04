f323fea1339f5981795ada912a1e60e6cd7f22ed
Brian Carrier <carrier@sleuthkit.org>
2015-09-03 22:40:01 -0400

    Added method tsk_fs_dir_get_name for finding files in a directory faster

--- tsk/fs/tsk_fs.h
+++ tsk/fs/tsk_fs.h
@@ -652,6 +652,7 @@ extern "C" {
         void *a_ptr);
     extern size_t tsk_fs_dir_getsize(const TSK_FS_DIR *);
     extern TSK_FS_FILE *tsk_fs_dir_get(const TSK_FS_DIR *, size_t);
+    extern const TSK_FS_NAME *tsk_fs_dir_get_name(const TSK_FS_DIR * a_fs_dir, size_t a_idx);
     extern void tsk_fs_dir_close(TSK_FS_DIR *);
 
     extern int8_t tsk_fs_path2inum(TSK_FS_INFO * a_fs, const char *a_path,
