651a3955fad5c5867000a63fbc37297bd684ecfe
Eugene Livis <elivis@basistech.com>
2015-12-09 17:21:59 -0500

    Started work on vmdk_open()

--- tsk/img/vmdk.c
+++ tsk/img/vmdk.c
@@ -119,7 +119,7 @@ static void
         tsk_error_set_errstr("vmdk_image_close: unable to free handle - %s", errmsg);
     }
 
-    // ELTODO: this stuff crashes in libewf. keep an eye. See ewf.c.
+    // ELTODO: this stuff crashes in libvmdk. keep an eye. See vmdk.c.
     for (i = 0; i < vmdk_info->num_imgs; i++) {
         free(vmdk_info->images[i]);
     }
@@ -129,5 +129,274 @@ static void
     tsk_img_free(img_info);
 }
 
+
+
+TSK_IMG_INFO *
+vmdk_open(int a_num_img,
+    const TSK_TCHAR * const a_images[], unsigned int a_ssize)
+{
+    char error_string[TSK_VMDK_ERROR_STRING_SIZE];
+    libcerror_error_t *vmdk_error = NULL;
+    int result = 0;
+
+    IMG_VMDK_INFO *vmdk_info = NULL;
+    TSK_IMG_INFO *img_info = NULL;
+
+    if (tsk_verbose) {
+        libvmdk_notify_set_verbose(1);
+        libvmdk_notify_set_stream(stderr, NULL);    // ELTODO: stderr
+    }
+
+    if ((vmdk_info =
+            (IMG_VMDK_INFO *) tsk_img_malloc(sizeof(IMG_VMDK_INFO))) ==
+        NULL) {
+        return NULL;
+    }
+    img_info = (TSK_IMG_INFO *) vmdk_info;
+
+    // See if they specified only the first of the set...    
+    if (a_num_img == 1) {
+        // ELTODO: ewf calls some kind of "glob" here to figure out number of segments.
+        // Perhaps use libvmdk_handle_get_number_of_extents()?
+        /*if (tsk_verbose)
+            tsk_fprintf(stderr,
+                "vmdk_open: found %d segment files via libvmdk_glob\n",
+                vmdk_info->num_imgs);*/
+    }
+    else {
+        int i;
+        vmdk_info->num_imgs = a_num_img;
+        if ((vmdk_info->images =
+                (TSK_TCHAR **) tsk_malloc(a_num_img *
+                    sizeof(TSK_TCHAR *))) == NULL) {
+            tsk_img_free(vmdk_info);
+            return NULL;
+        }
+        for (i = 0; i < a_num_img; i++) {
+            if ((vmdk_info->images[i] =
+                    (TSK_TCHAR *) tsk_malloc((TSTRLEN(a_images[i]) +
+                            1) * sizeof(TSK_TCHAR))) == NULL) {
+                tsk_img_free(vmdk_info);
+                return NULL;
+            }
+            TSTRNCPY(vmdk_info->images[i], a_images[i],
+                TSTRLEN(a_images[i]) + 1);
+        }
+    }
+
+
+#if defined( HAVE_LIBvmdk_V2_API )
+
+    // Check the file signature before we call the library open
+#if defined( TSK_WIN32 )
+    if (libvmdk_check_file_signature_wide(a_images[0], &vmdk_error) != 1)
+#else
+    if (libvmdk_check_file_signature(a_images[0], &vmdk_error) != 1)
+#endif
+    {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_MAGIC);
+
+        getError(vmdk_error, error_string);
+        tsk_error_set_errstr("vmdk_open: Not an vmdk file (%s)",
+            error_string);
+        libvmdk_error_free(&vmdk_error);
+
+        tsk_img_free(vmdk_info);
+
+        if (tsk_verbose != 0) {
+            tsk_fprintf(stderr, "Not an vmdk file\n");
+        }
+        return (NULL);
+    }
+
+    if (libvmdk_handle_initialize(&(vmdk_info->handle), &vmdk_error) != 1) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_OPEN);
+
+        getError(vmdk_error, error_string);
+        tsk_error_set_errstr("vmdk_open file: %" PRIttocTSK
+            ": Error initializing handle (%s)", a_images[0], error_string);
+        libvmdk_error_free(&vmdk_error);
+
+        tsk_img_free(vmdk_info);
+
+        if (tsk_verbose != 0) {
+            tsk_fprintf(stderr, "Unable to create vmdk handle\n");
+        }
+        return (NULL);
+    }
+#if defined( TSK_WIN32 )
+    if (libvmdk_handle_open_wide(vmdk_info->handle,
+            (wchar_t * const *) vmdk_info->images,
+            vmdk_info->num_imgs, LIBvmdk_OPEN_READ, &vmdk_error) != 1)
+#else
+    if (libvmdk_handle_open(vmdk_info->handle,
+            (char *const *) vmdk_info->images,
+            vmdk_info->num_imgs, LIBvmdk_OPEN_READ, &vmdk_error) != 1)
+#endif
+    {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_OPEN);
+
+        getError(vmdk_error, error_string);
+        tsk_error_set_errstr("vmdk_open file: %" PRIttocTSK
+            ": Error opening (%s)", a_images[0], error_string);
+        libvmdk_error_free(&vmdk_error);
+
+        tsk_img_free(vmdk_info);
+
+        if (tsk_verbose != 0) {
+            tsk_fprintf(stderr, "Error opening vmdk file\n");
+        }
+        return (NULL);
+    }
+    if (libvmdk_handle_get_media_size(vmdk_info->handle,
+            (size64_t *) & (img_info->size), &vmdk_error) != 1) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_OPEN);
+
+        getError(vmdk_error, error_string);
+        tsk_error_set_errstr("vmdk_open file: %" PRIttocTSK
+            ": Error getting size of image (%s)", a_images[0],
+            error_string);
+        libvmdk_error_free(&vmdk_error);
+
+        tsk_img_free(vmdk_info);
+
+        if (tsk_verbose != 0) {
+            tsk_fprintf(stderr, "Error getting size of vmdk file\n");
+        }
+        return (NULL);
+    }
+    result = libvmdk_handle_get_utf8_hash_value_md5(vmdk_info->handle,
+        (uint8_t *) vmdk_info->md5hash, 33, &vmdk_error);
+
+    if (result == -1) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_OPEN);
+
+        getError(vmdk_error, error_string);
+        tsk_error_set_errstr("vmdk_open file: %" PRIttocTSK
+            ": Error getting MD5 of image (%s)", a_images[0],
+            error_string);
+        libvmdk_error_free(&vmdk_error);
+
+        tsk_img_free(vmdk_info);
+
+        if (tsk_verbose != 0) {
+            tsk_fprintf(stderr, "Error getting size of vmdk file\n");
+        }
+        return (NULL);
+    }
+    vmdk_info->md5hash_isset = result;
+
+#else                           // V1 API
+
+    // Check the file signature before we call the library open
+#if defined( TSK_WIN32 )
+    if (libvmdk_check_file_signature_wide(a_images[0]) != 1)
+#else
+    if (libvmdk_check_file_signature(a_images[0]) != 1)
+#endif
+    {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_MAGIC);
+        tsk_error_set_errstr("vmdk_open: Not an vmdk file");
+        tsk_img_free(vmdk_info);
+        if (tsk_verbose)
+            tsk_fprintf(stderr, "Not an vmdk file\n");
+
+        return NULL;
+    }
+
+#if defined( TSK_WIN32 )
+    vmdk_info->handle = libvmdk_open_wide(
+        (wchar_t * const *) vmdk_info->images, vmdk_info->num_imgs,
+        LIBvmdk_OPEN_READ);
+#else
+    vmdk_info->handle = libvmdk_open(
+        (char *const *) vmdk_info->images, vmdk_info->num_imgs,
+        LIBvmdk_OPEN_READ);
+#endif
+    if (vmdk_info->handle == NULL) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_OPEN);
+        tsk_error_set_errstr("vmdk_open file: %" PRIttocTSK
+            ": Error opening", vmdk_info->images[0]);
+        tsk_img_free(vmdk_info);
+
+        if (tsk_verbose != 0) {
+            tsk_fprintf(stderr, "Error opening vmdk file\n");
+        }
+        return (NULL);
+    }
+#if defined( LIBvmdk_STRING_DIGEST_HASH_LENGTH_MD5 )
+    // 2007 version
+    img_info->size = libvmdk_get_media_size(vmdk_info->handle);
+
+    vmdk_info->md5hash_isset = libvmdk_get_stored_md5_hash(vmdk_info->handle,
+        vmdk_info->md5hash, LIBvmdk_STRING_DIGEST_HASH_LENGTH_MD5);
+#else
+    // libvmdk-20080322 version
+    if (libvmdk_get_media_size(vmdk_info->handle,
+            (size64_t *) & (img_info->size)) != 1) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_IMG_OPEN);
+        tsk_error_set_errstr("vmdk_open file: %" PRIttocTSK
+            ": Error getting size of image", vmdk_info->images[0]);
+        tsk_img_free(vmdk_info);
+        if (tsk_verbose) {
+            tsk_fprintf(stderr, "Error getting size of vmdk file\n");
+        }
+        return (NULL);
+    }
+    if (libvmdk_get_md5_hash(vmdk_info->handle, md5_hash, 16) == 1) {
+        int md5_string_iterator = 0;
+        int md5_hash_iterator = 0;
+
+        for (md5_hash_iterator = 0;
+            md5_hash_iterator < 16; md5_hash_iterator++) {
+            int digit = md5_hash[md5_hash_iterator] / 16;
+
+            if (digit <= 9) {
+                vmdk_info->md5hash[md5_string_iterator++] =
+                    '0' + (char) digit;
+            }
+            else {
+                vmdk_info->md5hash[md5_string_iterator++] =
+                    'a' + (char) (digit - 10);
+            }
+            digit = md5_hash[md5_hash_iterator] % 16;
+
+            if (digit <= 9) {
+                vmdk_info->md5hash[md5_string_iterator++] =
+                    '0' + (char) digit;
+            }
+            else {
+                vmdk_info->md5hash[md5_string_iterator++] =
+                    'a' + (char) (digit - 10);
+            }
+        }
+        vmdk_info->md5hash_isset = 1;
+    }
+#endif                          /* defined( LIBvmdk_STRING_DIGEST_HASH_LENGTH_MD5 ) */
+#endif                          /* defined( HAVE_LIBvmdk_V2_API ) */
+    if (a_ssize != 0) {
+        img_info->sector_size = a_ssize;
+    }
+    else {
+        img_info->sector_size = 512;
+    }
+    img_info->itype = TSK_IMG_TYPE_vmdk_vmdk;
+    img_info->read = &vmdk_image_read;
+    img_info->close = &vmdk_image_close;
+    img_info->imgstat = &vmdk_image_imgstat;
+
+    // initialize the read lock
+    tsk_init_lock(&(vmdk_info->read_lock));
+
+    return (img_info);
+}
 
 #endif /* HAVE_LIBVMDK */
