sleuthkit/sleuthkit

commit 3ba58ccdefeaba41274ee6182a7fcf4a84c543a8

Ensure mac partition tables have at least one valid partition

tsk/vs/mac.c
@@ -148,10 +148,16 @@ mac_load_table(TSK_VS_INFO * vs)
         if (NULL == tsk_vs_part_add(vs, (TSK_DADDR_T) part_start,
                 (TSK_DADDR_T) part_size, (TSK_VS_PART_FLAG_ENUM)flag, str, -1,
                 idx))
+            free(part_buf);
             return 1;
     }
     free(part_buf);
     part_buf = NULL;
+
+    // Bail if we did find any valid entries
+    if (vs->part_count == 0) {
+        return 1;
+    }
 
     /* Add an entry for the table length */
     if ((table_str = tsk_malloc(16)) == NULL) {

commit 25dc16df225fb6284a8bedfd0018c3819b4f4506

Missing braces for this conditional causes 'return 1' to happen unconditionally. Fixes bug introduced in 3ba58ccd.

tsk/vs/mac.c
@@ -147,9 +147,10 @@ mac_load_table(TSK_VS_INFO * vs)
 
         if (NULL == tsk_vs_part_add(vs, (TSK_DADDR_T) part_start,
                 (TSK_DADDR_T) part_size, (TSK_VS_PART_FLAG_ENUM)flag, str, -1,
-                idx))
+                idx)) {
             free(part_buf);
             return 1;
+        }
     }
     free(part_buf);
     part_buf = NULL;