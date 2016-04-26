e961f61acab0ac1b5ea5d8409d74a956b9191347
esaunders <esaunders@basistech.com>
2016-04-26 10:35:19 -0400

    Initialize alloc_file_count to -1.

--- tsk/fs/ntfs_dent.cpp
+++ tsk/fs/ntfs_dent.cpp
@@ -212,7 +212,10 @@ ntfs_parent_act(TSK_FS_FILE * fs_file, void *ptr)
 
     if ((fs_file->meta->flags & TSK_FS_META_FLAG_ALLOC) &&
         fs_file->meta->type == TSK_FS_META_TYPE_REG) {
-        ntfs->alloc_file_count++;
+            if (ntfs->alloc_file_count == -1)
+                ntfs->alloc_file_count = 1;
+            else
+                ntfs->alloc_file_count++;
     }
 
     /* go through each file name structure */
