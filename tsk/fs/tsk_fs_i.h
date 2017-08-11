bed3ca3458694bbb6de91a657af6fd2526c0a62d
Ann Priestman <apriestman@basistech.com>
2017-08-11 12:40:36 -0400

    Fixed fatfs_istat declaration. Added istat flag to tsk_fs_nofs_istat.

--- tsk/fs/tsk_fs_i.h
+++ tsk/fs/tsk_fs_i.h
@@ -220,7 +220,7 @@ extern "C" {
         TSK_INUM_T a_start_inum, TSK_INUM_T a_end_inum,
         TSK_FS_META_FLAG_ENUM a_flags, TSK_FS_META_WALK_CB a_action,
         void *a_ptr);
-    extern uint8_t tsk_fs_nofs_istat(TSK_FS_INFO * a_fs, FILE * hFile,
+    extern uint8_t tsk_fs_nofs_istat(TSK_FS_INFO * a_fs, TSK_FS_ISTAT_FLAG_ENUM istat_flags, FILE * hFile,
         TSK_INUM_T inum, TSK_DADDR_T numblock, int32_t sec_skew);
     extern TSK_RETVAL_ENUM tsk_fs_nofs_dir_open_meta(TSK_FS_INFO * a_fs,
         TSK_FS_DIR ** a_fs_dir, TSK_INUM_T a_addr);
