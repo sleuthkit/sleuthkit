693113862664dd2d64e4b30197be713bccf20d29
Ann Priestman <apriestman@basistech.com>
2017-12-01 11:57:47 -0500

    Cleanup

--- tools/fstools/fls.cpp
+++ tools/fstools/fls.cpp
@@ -214,9 +214,6 @@ main(int argc, char **argv1)
         tsk_fprintf(stderr, "Missing image name\n");
         usage();
     }
-    
-    printf("\n#### THIS IS MY BRANCH 2 ####\n\n");
-    fflush(stdout);
 
     /* Set the full flag to print the full path name if recursion is
      ** set and we are only displaying files or deleted files
