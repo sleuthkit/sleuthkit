a51f361761090ae66a9eff7298a66ae65f16535f
rishwanth1995 <rishwanth1995@hotmail.com>
2018-03-27 14:40:46 -0400

    modified to .travis.yml to help in autopsy build

--- install-sleuthkit.sh
+++ install-sleuthkit.sh
@@ -2,11 +2,7 @@
 set -ex
 if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
-	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev libsqlite3-dev testdisk ant libcppunit-dev	
+	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev libsqlite3-dev ant libcppunit-dev	
 fi
 ./bootstrap && ./configure --prefix=/usr && make
-if test ${TRAVIS_REPO_SLUG} = "sleuthkit/autopsy"; then
-	cd bindings/java/dist
-	ln -s sleuthkit-4.6.0.jar sleuthkit-postgresql-4.6.0.jar
-fi
- 
+cd bindings/java/ && ant dist-PostgreSQL
