2550026c66a6553817b00868948115fc6055c0a1
rishwanth1995 <rishwanth1995@hotmail.com>
2018-03-27 11:52:42 -0400

    travis-testing-3

--- install-sleuthkit.sh
+++ install-sleuthkit.sh
@@ -4,10 +4,9 @@ if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
 	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev libsqlite3-dev testdisk ant libcppunit-dev	
 fi
-#if test ${TRAVIS_REPO_SLUG} = "sleuthkit/autopsy"; then
-#	git clone https://github.com/sleuthkit/sleuthkit
-#	cd sleuthkit
-#fi
 ./bootstrap && ./configure --prefix=/usr && make
-#cd /usr/share/java && sudo ln -s sleuthkit-4.6.0.jar sleuthkit-postgresql-4.6.0.jar
+if test ${TRAVIS_REPO_SLUG} = "sleuthkit/autopsy"; then
+	cd bindings/java/dist
+	ln -s sleuthkit-4.6.0.jar sleuthkit-postgresql-4.6.0.jar
+fi
  
