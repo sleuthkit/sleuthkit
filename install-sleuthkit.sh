c6b16d502e2c93751f8a4f41b5451d17003f6bba
rishwanth1995 <rishwanth1995@hotmail.com>
2018-03-27 10:51:13 -0400

    added travis script

---
+++ install-sleuthkit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+if test ${TRAVIS_OS_NAME} = "linux"; then
+	sudo apt-get --qq update
+	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev postgresql-9.5 libvhdi-dev libvmdk-dev libsqlite3-dev testdisk ant libcppunit-dev	
+fi
+#if test ${TRAVIS_REPO_SLUG} = "sleuthkit/autopsy"; then
+#	git clone https://github.com/sleuthkit/sleuthkit
+#	cd sleuthkit
+#fi
+./bootstrap && ./configure --prefix=/usr && make
+#cd /usr/share/java && sudo ln -s sleuthkit-4.6.0.jar sleuthkit-postgresql-4.6.0.jar
+ 
