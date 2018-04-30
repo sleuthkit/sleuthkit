76e12d5b2e2657b9ece4e5b8021f3cee7c0198ac
rishwanth1995 <rishwanthsenthilkumar@hotmail.com>
2018-04-30 13:16:37 -0400

    compile only dll project

--- win32/updateAndBuildAll.py
+++ win32/updateAndBuildAll.py
@@ -177,7 +177,6 @@ def buildDependentLibs(libHome, wPlatform, targetDll,project):
     elif wPlatform == 32:
         vs.append("/p:platform=Win32")
     vs.append("/v:quiet")
-    vs.append("/t:build")
 
     outputFile = os.path.join(LOG_PATH, targetDll + "Output.txt")
     VSout = open(outputFile, 'w')
