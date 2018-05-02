2c5a6a20fdbb383aefdbd86aaede83eda4bc96e0
rishwanth1995 <rishwanthsenthilkumar@hotmail.com>
2018-05-02 14:46:40 -0400

    travis testing

--- travis_build.sh
+++ travis_build.sh
@@ -11,9 +11,9 @@ installLib() {
 
 if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
-	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev libsqlite3-dev ant libcppunit-dev
+	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev
 elif test ${TRAVIS_OS_NAME} = "osx"; then
-	brew install ant
+	brew install ant gettext
 fi
 installLib libvhdi
 installLib libvmdk
