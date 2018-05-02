79b7100ff1b1a80b2a124295657d055cd839c9e2
rishwanth1995 <rishwanthsenthilkumar@hotmail.com>
2018-05-02 14:38:55 -0400

    travis testing

--- travis_build.sh
+++ travis_build.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 set -ex
+installLib() {
+	git clone https://github.com/libyal/$1
+	cd $1
+	./synclibs.sh
+	./autogen.sh
+	./configure && make > /dev/null && make install > /dev/null
+	cd ..
+}
+
 if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
 	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev libsqlite3-dev ant libcppunit-dev
 elif test ${TRAVIS_OS_NAME} = "osx"; then
-	brew install ant	
+	brew install ant
 fi
+installLib libvhdi
+installLib libvmdk
 ./bootstrap && ./configure --prefix=/usr && make > /dev/null
 cd bindings/java/ && ant dist-PostgreSQL
