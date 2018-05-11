e289307f2aa869122c3aee8ca2000378b52915b1
rishwanth1995 <rishwanthsenthilkumar@hotmail.com>
2018-05-11 14:29:39 -0400

    travis testing

--- travis_build.sh
+++ travis_build.sh
@@ -19,7 +19,7 @@ installLib() {
 
 if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
-	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev
+	sudo apt-get -y install autoconf automake perl libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev
 elif test ${TRAVIS_OS_NAME} = "osx"; then
 	export PATH=${PATH}:/usr/local/opt/gettext/bin
 	brew install ant libewf gettext
