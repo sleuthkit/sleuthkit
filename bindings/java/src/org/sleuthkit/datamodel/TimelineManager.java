ac9cd8e6b1b2fe39daf26996c91e654c8a9805f3
millmanorama <millmanorama@gmail.com>
2018-07-18 16:48:59 +0200

    remove unused import

--- bindings/java/src/org/sleuthkit/datamodel/TimelineManager.java
+++ bindings/java/src/org/sleuthkit/datamodel/TimelineManager.java
@@ -25,7 +25,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
