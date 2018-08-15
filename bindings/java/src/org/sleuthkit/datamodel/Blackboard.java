60a1075b3d39b917f5072b553567958c6ab7df18
millmanorama <millmanorama@gmail.com>
2018-08-15 11:21:07 +0200

    remove unused import

--- bindings/java/src/org/sleuthkit/datamodel/Blackboard.java
+++ bindings/java/src/org/sleuthkit/datamodel/Blackboard.java
@@ -29,7 +29,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
