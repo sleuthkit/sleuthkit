e9fa4d5336044709c7520a782bf66bef17b9dfd7
U-BASIS\dsmyda <dsmyda@win-dsmyd-4990.basistech.net>
2018-10-04 10:28:23 -0400

    Ended up running formatting on it

--- bindings/java/src/org/sleuthkit/datamodel/EncodedFileOutputStream.java
+++ bindings/java/src/org/sleuthkit/datamodel/EncodedFileOutputStream.java
@@ -1,15 +1,15 @@
 /*
  * SleuthKit Java Bindings
- * 
+ *
  * Copyright 2011-2016 Basis Technology Corp.
  * Contact: carrier <at> sleuthkit <dot> org
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,61 +23,67 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * Output stream wrapper for encoding files being written to disk.
- * The idea is to prevent malicious files from getting extracted onto
- * the user's hard drive in their original form. The encoding type used here
- * should match the one used to create the derived file database entry for this file.
+ * Output stream wrapper for encoding files being written to disk. The idea is
+ * to prevent malicious files from getting extracted onto the user's hard drive
+ * in their original form. The encoding type used here should match the one used
+ * to create the derived file database entry for this file.
  */
-public class EncodedFileOutputStream extends BufferedOutputStream{
+public class EncodedFileOutputStream extends BufferedOutputStream {
+
 	private TskData.EncodingType type;
-	
+
 	/**
 	 * Create an encoded output stream using the specified encoding.
+	 *
 	 * @param out
 	 * @param type
-	 * @throws IOException 
+	 *
+	 * @throws IOException
 	 */
-	public EncodedFileOutputStream(OutputStream out, TskData.EncodingType type) throws IOException{
-        super(out);
+	public EncodedFileOutputStream(OutputStream out, TskData.EncodingType type) throws IOException {
+		super(out);
 		this.type = type;
-        writeHeader();		
+		writeHeader();
 	}
 
 	/**
-	 * Create an encoded output stream using the specified encoding and buffer size.
+	 * Create an encoded output stream using the specified encoding and buffer
+	 * size.
+	 *
 	 * @param out
 	 * @param size
 	 * @param type
-	 * @throws IOException 
+	 *
+	 * @throws IOException
 	 */
-    public EncodedFileOutputStream(OutputStream out, int size, TskData.EncodingType type) throws IOException{
-        super(out, size);
+	public EncodedFileOutputStream(OutputStream out, int size, TskData.EncodingType type) throws IOException {
+		super(out, size);
 		this.type = type;
-        writeHeader();
-    }
-    
-    private void writeHeader() throws IOException{
+		writeHeader();
+	}
+
+	private void writeHeader() throws IOException {
 		// We get the encoded header here so it will be in plaintext after encoding
 		write(EncodedFileUtil.getEncodedHeader(type), 0, EncodedFileUtil.getHeaderLength());
-    }
-    
-    @Override
-    public void write(int b) throws IOException{
-        super.write((int)EncodedFileUtil.encodeByte((byte)b, type));
-    }
-    
-    @Override
-    public void write(byte[] b,
-                  int off,
-                  int len)
-           throws IOException{
-        byte[] encodedData = new byte[b.length];
-        for(int i = 0;i < b.length;i++){
-            encodedData[i] = EncodedFileUtil.encodeByte(b[i], type);
-        }
-        
-        super.write(encodedData, off, len);
-	//encodedData not gc'd until nulled out.
-	encodedData = null;
-    }	
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		super.write((int) EncodedFileUtil.encodeByte((byte) b, type));
+	}
+
+	@Override
+	public void write(byte[] b,
+			int off,
+			int len)
+			throws IOException {
+		byte[] encodedData = new byte[b.length];
+		for (int i = 0; i < b.length; i++) {
+			encodedData[i] = EncodedFileUtil.encodeByte(b[i], type);
+		}
+
+		super.write(encodedData, off, len);
+		//encodedData not gc'd until nulled out.
+		encodedData = null;
+	}
 }
