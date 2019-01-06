3bc600dd27be1e730b80d74928e446046068e2ca
Joe Sylve <Joe.Sylve@gmail.com>
2019-01-05 19:55:09 -0600

    Update travis_build.sh

--- travis_build.sh
+++ travis_build.sh
@@ -15,12 +15,12 @@ installLib() {
 
 if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
-	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev wget
+	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev wget openssl-dev
 elif test ${TRAVIS_OS_NAME} = "osx"; then
 	export PATH=${PATH}:/usr/local/opt/gettext/bin
-	brew install ant libewf gettext cppunit afflib
+	brew install ant libewf gettext cppunit afflib openssl
 fi
-installLib libvhdi 20181125
-installLib libvmdk 20181124
+installLib libvhdi 20181227
+installLib libvmdk 20181227
 ./bootstrap && ./configure --prefix=/usr && make > /dev/null
 cd bindings/java/ && ant -q dist-PostgreSQL
