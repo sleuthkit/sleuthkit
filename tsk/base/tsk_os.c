a58b1f7506282bb12c129b32701522f5e079f684
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2019-03-12 14:41:28 +0000

    Moved strnlen implementation to tsk_os.c.

---
+++ tsk/base/tsk_os.c
@@ -0,0 +1,21 @@
+/*
+ * The Sleuth Kit
+ *
+ * Brian Carrier [carrier <at> sleuthkit [dot] org]
+ * Copyright (c) 2006-2011 Brian Carrier.  All Rights reserved
+ *
+ * This software is distributed under the Common Public License 1.0
+ */
+
+#include "tsk_base_i.h"
+#include "tsk_base.h"
+
+#include <string.h>
+
+#ifndef HAVE_STRNLEN
+size_t
+strnlen(const char* s, size_t maxlen) {
+  const char* z = memchr(s, 0, maxlen);
+  return z ? z-s : n;
+}
+#endif
