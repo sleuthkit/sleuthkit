de4915e2a445c86a3666b971a2950322d838fe12
Joe Ho <jkho@basistech.com>
2019-03-14 09:48:01 -0400

    4802 handle \\.\PhysicalDrive0

--- tools/autotools/tsk_logical_imager.cpp
+++ tools/autotools/tsk_logical_imager.cpp
@@ -9,14 +9,145 @@
  **
  */
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <locale>
+#include <codecvt>
 #include <direct.h>
 #include <winsock2.h>
 #include "tsk/tsk_tools_i.h"
 #include "tsk/auto/tsk_case_db.h"
 #include "tsk/img/img_writer.h"
 #include <locale.h>
+#include <Wbemidl.h>
+#pragma comment(lib, "wbemuuid.lib")
+
+#include <comutil.h>
+
+using namespace std;
+
+std::wstring GetLastErrorStdStrW();
+std::string GetErrorStdStr(DWORD err);
+std::wstring GetErrorStdStrW(DWORD err);
 
 static TSK_TCHAR *progname;
+
+static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+
+string toUpper(const string &srcStr)
+{
+	string outStr(srcStr);
+	std::transform(srcStr.begin(), srcStr.end(), outStr.begin(), ::toupper);
+
+	return outStr;
+}
+string toLower(const string &srcStr)
+{
+	string outStr(srcStr);
+	std::transform(srcStr.begin(), srcStr.end(), outStr.begin(), ::tolower);
+
+	return outStr;
+}
+
+/**
+* Convert from UTF-16 to UTF-8.
+* Returns empty string on error
+*/
+string toNarrow(const wstring& a_utf16Str)
+{
+	try {
+		std::string narrow = converter.to_bytes(a_utf16Str);
+		return narrow;
+	}
+	catch (...) {
+		std::exception_ptr eptr = std::current_exception();
+		return "";
+	}
+}
+
+/**
+* Convert from UTF-8 to UTF-16.
+* Returns empty string on error
+*/
+wstring toWide(const string& a_utf8Str)
+{
+	try {
+		std::wstring wide = converter.from_bytes(a_utf8Str);
+		return wide;
+	}
+	catch (...) {
+		std::exception_ptr eptr = std::current_exception();
+		return L"";
+	}
+}
+
+/**
+* GetErrorStdStr - returns readable error message for the given error code
+*
+* @param err error code
+* @returns error message string
+*/
+string GetErrorStdStr(DWORD err) {
+	return toNarrow(GetErrorStdStrW(err));
+}
+
+/**
+* GetLastErrorStdStrW - returns readable widestring error message for the last error code as reported by GetLastError()
+*
+* @returns error message wide string
+*/
+wstring GetLastErrorStdStrW() {
+	DWORD error = GetLastError();
+	return GetErrorStdStrW(error);
+}
+
+/**
+* GetErrorStdStrW - returns readable widestring error message for the given error code
+*
+* @param err error code
+* @returns error message wide string
+*/
+wstring GetErrorStdStrW(DWORD a_err)
+{
+	if (ERROR_SUCCESS != a_err)
+	{
+		LPVOID lpMsgBuf;
+		DWORD bufLen = FormatMessageW(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			a_err,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&lpMsgBuf,
+			0, NULL);
+		if (bufLen)
+		{
+			LPCWSTR lpMsgStr = (LPCWSTR)lpMsgBuf;
+
+			/***
+			LPWSTR p = const_cast<LPWSTR>(_tcschr(lpMsgStr, _T('\r')));
+			if(p != NULL) { // lose CRLF
+			*p = _T('\0');
+			}
+			****/
+
+			std::wstring result(lpMsgStr, lpMsgStr + bufLen);
+
+			size_t pos = result.find_last_not_of(L"\r\n");
+			if (pos != std::wstring::npos) {
+				result.resize(pos);
+			}
+
+
+			LocalFree(lpMsgBuf);
+
+			return result;
+		}
+	}
+	return std::wstring(L"no error");
+}
 
 static void
 usage()
@@ -33,7 +164,7 @@ usage()
 }
 
 // is Windows XP or older?
-bool isWinXPOrOlder() {
+BOOL isWinXPOrOlder() {
 	OSVERSIONINFO	vi;
 	memset(&vi, 0, sizeof vi);
 	vi.dwOSVersionInfoSize = sizeof vi;
@@ -44,7 +175,7 @@ bool isWinXPOrOlder() {
 	return((m_winntVerMajor <= 5));
 }
 
-static BOOL IsProcessElevated() {
+static BOOL isProcessElevated() {
 	static BOOL fRet = FALSE;
 	HANDLE hToken = NULL;
 
@@ -118,6 +249,380 @@ int createDirectory(string &directoryPathname) {
 	return 0;
 }
 
+/**
+* wmi_init: Initialize WMN
+*
+* @param input wmiNamespace - wmi namespace to open
+* @returns  0 on success
+*			WBEM_E_INVALID_NAMESPACE, if namespace is not found
+*           -1 if error
+*
+* Ref: https://msdn.microsoft.com/en-us/library/aa390423(VS.85).aspx
+*
+*/
+
+long wmi_init(const wstring& wmiNamespace, IWbemLocator **ppWbemLocator, IWbemServices **ppWbemServices)
+{
+	HRESULT hres;
+
+	// Step 1: Initialize COM.
+
+	hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+	if (FAILED(hres))
+	{
+		fprintf(stderr, "wmi_init: Failed to initialize COM library. Error code = %#X\n", hres);
+		return -1;                  // Program has failed.
+	}
+
+	// Step 2: Set general COM security levels
+	hres = CoInitializeSecurity(
+		NULL,
+		-1,                          // COM authentication
+		NULL,                        // Authentication services
+		NULL,                        // Reserved
+		RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+		NULL,                        // Authentication info
+		EOAC_NONE,                   // Additional capabilities 
+		NULL                         // Reserved
+	);
+
+	if (FAILED(hres))
+	{
+		fprintf(stderr, "wmi_init: Failed to initialize security. Error code = %#X\n", hres);
+		CoUninitialize();
+		return -1;                    // Program has failed.
+	}
+
+	// Step 3: Obtain the initial locator to WMI
+	hres = CoCreateInstance(
+		CLSID_WbemLocator,
+		0,
+		CLSCTX_INPROC_SERVER,
+		IID_IWbemLocator, (LPVOID *)ppWbemLocator);
+
+	if (FAILED(hres))
+	{
+		fprintf(stderr, "wmi_init: Failed to create IWbemLocator object. Err code = %#X\n", hres);
+		CoUninitialize();
+		return -1;                 // Program has failed.
+	}
+
+	// Step 4: Connect to WMI through the IWbemLocator::ConnectServer method
+	// Connect to the given namespace with
+	// the current user and obtain pointer pSvc
+	// to make IWbemServices calls.
+	hres = (*ppWbemLocator)->ConnectServer(
+		_bstr_t(wmiNamespace.c_str()), // Object path of WMI namespace
+		NULL,                    // User name. NULL = current user
+		NULL,                    // User password. NULL = current
+		0,                       // Locale. NULL indicates current
+		NULL,                    // Security flags.
+		0,                       // Authority (e.g. Kerberos)
+		0,                       // Context object 
+		ppWbemServices                    // pointer to IWbemServices proxy
+	);
+
+	if (FAILED(hres))
+	{
+		if (WBEM_E_INVALID_NAMESPACE != hres) {
+			fprintf(stderr, "wmi_init: Could not connect to namespace %s, Error = %s\n", toNarrow(wmiNamespace).c_str(), GetErrorStdStr(hres).c_str());
+		}
+
+		(*ppWbemLocator)->Release();
+		CoUninitialize();
+
+		return (WBEM_E_INVALID_NAMESPACE == hres) ? hres : -1;
+	}
+
+	// Step 5: Set security levels on the proxy
+	hres = CoSetProxyBlanket(
+		*ppWbemServices,                        // Indicates the proxy to set
+		RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+		RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+		NULL,                        // Server principal name 
+		RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+		RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+		NULL,                        // client identity
+		EOAC_NONE                    // proxy capabilities 
+	);
+
+	if (FAILED(hres))
+	{
+		fprintf(stderr, "wmi_init: Could not set proxy blanket. Error code = %#X\n", hres);
+		(*ppWbemServices)->Release();
+		(*ppWbemLocator)->Release();
+		CoUninitialize();
+		return -1;               // Program has failed.
+	}
+	return 0;
+}
+
+/**
+* wmi_close: closes WMI
+*
+* @returns  0 on success
+*           -1 if error
+*
+*/
+int wmi_close(IWbemLocator **ppWbemLocator, IWbemServices **ppWbemServices)
+{
+	// Cleanup
+	// ========
+
+	(*ppWbemServices)->Release();
+	(*ppWbemLocator)->Release();
+	CoUninitialize();
+
+	(*ppWbemServices) = NULL;
+	(*ppWbemLocator) = NULL;
+
+	return 0;
+}
+
+/**
+* checkDriveForLDM: checks if the given drive is an LDM disk
+*
+* @param input driveLetter drive to check
+*
+* @returns  0 if the drive is NOT an LDM disk
+*           1 if the drive IS an LDM disk
+*           -1 if error, or if drive not found
+*
+*/
+int checkDriveForLDM(const string& driveLetter) {
+
+	IWbemLocator *pWbemLocator = NULL;
+	IWbemServices *pWbemServices = NULL;
+
+	if (0 != wmi_init(L"ROOT\\CIMV2", &pWbemLocator, &pWbemServices)) {
+		return -1;
+	}
+
+
+	// Use the IWbemServices pointer to make requests of WMI. 
+	// Make requests here:
+	HRESULT hres;
+	IEnumWbemClassObject* pEnumerator = NULL;
+	bool bDriveFound = false;
+	int isLDM = 0;
+
+	wstring wstrQuery = L"ASSOCIATORS OF {Win32_LogicalDisk.DeviceID='";
+	wstrQuery += toWide(driveLetter);
+	wstrQuery += L"'} where AssocClass=Win32_LogicalDiskToPartition";
+
+	// Run WMI query
+	hres = pWbemServices->ExecQuery(
+		bstr_t("WQL"),
+		bstr_t(wstrQuery.c_str()),
+		WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+		NULL,
+		&pEnumerator);
+
+	if (FAILED(hres)) {
+		cerr << "WMI Query for partition type failed. "
+			<< "Error code = 0x"
+			<< hex << hres << endl;
+		wmi_close(&pWbemLocator, &pWbemServices);
+		return -1;
+	}
+	else {
+
+		IWbemClassObject *pclsObj;
+		ULONG uReturn = 0;
+		while (pEnumerator) {
+			hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+			if (0 == uReturn) break;
+
+			VARIANT vtProp, vtProp2;
+
+			hres = pclsObj->Get(_bstr_t(L"Type"), 0, &vtProp, 0, 0);
+			wstring partitionType = vtProp.bstrVal;
+
+			hres = pclsObj->Get(_bstr_t(L"DeviceID"), 0, &vtProp2, 0, 0);
+			wstring deviceID = vtProp2.bstrVal;
+
+			VariantClear(&vtProp);
+			VariantClear(&vtProp2);
+
+			bDriveFound = true;
+
+			//wcout << L"Drive: " << toWide(driveLetter) << ", DeviceID:  " << deviceID << ", Type: " << partitionType << endl;
+			if (string::npos != toLower(toNarrow(partitionType)).find("logical disk manager")) {
+				cerr << "Found Logical Disk Manager disk for drive =   " << driveLetter << endl;
+
+				isLDM = 1;
+			}
+		}
+	}
+	pEnumerator->Release();
+
+	wmi_close(&pWbemLocator, &pWbemServices);
+
+	if (!bDriveFound) {
+		cerr << "Drive =  " << driveLetter << " not found in Win32_LogicalDiskToPartition" << endl;
+	}
+
+	return bDriveFound ? isLDM : -1;
+}
+
+/**
+* checkDriveForBitlocker: checks if the given drive has BitLocker encrypted
+*
+* @param input driveLetter drive to check
+*
+* @returns  0  if the drive is not encrypted
+*           1  if the drive is Bitlocker encrypted
+*           -1 if error
+*
+*/
+int checkDriveForBitlocker(const string& driveLetter) {
+
+	IWbemLocator *pWbemLocator = NULL;
+	IWbemServices *pWbemServices = NULL;
+
+	long rc = 0;
+
+	wstring wsBitLockerNamespace = L"ROOT\\CIMV2\\security\\MicrosoftVolumeEncryption";
+
+
+	// Init WMI with the requisite namespace. This may fail on some versions of Windows, if Bitlocker in not installed.
+	rc = wmi_init(wsBitLockerNamespace, &pWbemLocator, &pWbemServices);
+	if (0 != rc) {
+
+		if ((WBEM_E_INVALID_NAMESPACE == rc)) {
+			cerr << " Bitlocker is not installed." << endl;
+			return 0;
+		}
+		else {
+			cerr << "Failed to connect to WMI namespace = " << toNarrow(wsBitLockerNamespace) << endl;
+			return -1;
+		}
+	}
+
+	// Use the IWbemServices pointer to make requests of WMI. 
+	// Make requests here:
+	HRESULT hres;
+	IEnumWbemClassObject* pEnumerator = NULL;
+
+	unsigned int bitLockerStatus = 0; // assume no Bitlocker
+
+									  // WMI query
+	wstring wstrQuery = L"SELECT * FROM Win32_EncryptableVolume where driveletter = '";
+	wstrQuery += toWide(driveLetter);
+	wstrQuery += L"'";
+
+	// Run WMI query
+	hres = pWbemServices->ExecQuery(
+		bstr_t("WQL"),
+		bstr_t(wstrQuery.c_str()),
+		WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+		NULL,
+		&pEnumerator);
+
+	if (FAILED(hres)) {
+		cerr << "WMI Query for Win32_EncryptableVolume failed. "
+			<< "Error code = 0x"
+			<< hex << hres << endl;
+		wmi_close(&pWbemLocator, &pWbemServices);
+		return -1;
+	}
+	else {
+
+		IWbemClassObject *pclsObj;
+		ULONG uReturn = 0;
+		while (pEnumerator) {
+			hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+			if (0 == uReturn) break;
+
+			VARIANT vtProp;
+			hres = pclsObj->Get(_bstr_t(L"EncryptionMethod"), 0, &vtProp, 0, 0);
+
+			if (WBEM_E_NOT_FOUND == hres) { // Means Bitlocker is not installed
+				bitLockerStatus = 0;
+			}
+			else {
+				unsigned int encryptionMethod = vtProp.uintVal;
+				cerr << "Drive: " << driveLetter << ",  found in Win32_EncryptableVolume.  EncryptionMethod:  " << encryptionMethod << endl;
+
+				bitLockerStatus = (0 == encryptionMethod) ? 0 : 1;
+			}
+
+
+			VariantClear(&vtProp);
+		}
+	}
+	pEnumerator->Release();
+
+	wmi_close(&pWbemLocator, &pWbemServices);
+
+	return bitLockerStatus;
+}
+
+
+
+/**
+* getDriveToProcess() - returns the drive to process
+*          By default we process PhysicalDrive0, unless
+*          C: is paritioned with LDM or has Bitlocker enabled, in which case we process 'C:'
+*
+* @param output driveToProcess
+*
+* @returns  TRUE on success or FALSE in case of failure.
+*
+*
+*/
+BOOL getDriveToProcess(string& driveToProcess) {
+
+	// check if they are admin before we give them some ugly error messages
+	if (isProcessElevated() == FALSE) {
+		return FALSE;
+	}
+
+	int checkLDMStatus = 0;
+	int checkBitlockerStatus = 0;
+
+	// By default, cast a wide net
+	driveToProcess = "PhysicalDrive0";
+
+	const string systemDriveLetter = "C:";
+
+	// if C: is part of LDM, then we'll just analyze C:
+	checkLDMStatus = checkDriveForLDM(systemDriveLetter);
+	if (1 == checkLDMStatus) {
+		fprintf(stderr, "System drive %s is an LDM disk\n", systemDriveLetter.c_str());
+		driveToProcess = systemDriveLetter;
+		return TRUE;
+	}
+
+	// If bitlocker protection is enabled, then analyze C: and not full drive
+	checkBitlockerStatus = checkDriveForBitlocker(systemDriveLetter);
+	if (1 == checkBitlockerStatus) {
+		fprintf(stderr, "System drive %s is BitLocker encrypted\n", systemDriveLetter.c_str());
+
+		// record encrypted drive in target info
+//		setSystemdriveEncrypted(true);
+
+		driveToProcess = systemDriveLetter;
+		return TRUE;
+	}
+
+	if (0 == checkLDMStatus && 0 == checkBitlockerStatus) {
+		return TRUE;	// neither LDM nor BitLocker detected on C:
+	}
+	else { // an error happened  in determining LDM or ProtectionStatus
+		if (-1 == checkLDMStatus) {
+			fprintf(stderr, "Error in checking LDM disk\n");
+		}
+		if (-1 == checkBitlockerStatus) {
+			fprintf(stderr, "Error in checking BitLocker protection status\n");
+		}
+
+		// Take a chance and go after PhysicalDrive0,  few systems have LDM or Bitlocker 
+		return TRUE;
+	}
+}
+
 int
 main(int argc, char **argv1)
 {
@@ -127,7 +632,7 @@ main(int argc, char **argv1)
 	int ch;
 	TSK_TCHAR **argv;
 	unsigned int ssize = 0;
-	TSK_TCHAR *imgPath[1];
+	const TSK_TCHAR *imgPath[1];
 	BOOL iFlagUsed = FALSE;
 
 #ifdef TSK_WIN32
@@ -166,19 +671,34 @@ main(int argc, char **argv1)
 		}
 	}
 
+
+	wstring wImgPathName;
+
 	if (!iFlagUsed) {
-		if (!IsProcessElevated()) {
+		if (!isProcessElevated()) {
 			fprintf(stderr, "Process is not running in elevated mode\n");
 			exit(1);
 		}
+		string driveToProcess;
+		if (getDriveToProcess(driveToProcess)) {
+			fprintf(stderr, "driveToProcess = %s\n", driveToProcess.c_str());
+			wImgPathName = _TSK_T("\\\\.\\") + toWide(driveToProcess);
+			TFPRINTF(stdout, _TSK_T("wImgPathName = %s\n"), (TSK_TCHAR *)wImgPathName.c_str());
+			imgPath[0] = (TSK_TCHAR *)wImgPathName.c_str();
+		}
+		else {
+			fprintf(stderr, "getDriveToProcess failed\n");
+			exit(1);
+		}
 	}
+	TFPRINTF(stdout, _TSK_T("imgPath[0] = %s\n"), imgPath[0]);
 
 	// create a directory with hostname_timestamp
 	string directory_path;
 	if (createDirectory(directory_path) == -1) {
 		exit(1);
 	}
-	fprintf(stdout, "Created directory %s", directory_path.c_str());
+	fprintf(stdout, "Created directory %s\n", directory_path.c_str());
 
 	if ((img = tsk_img_open(1, imgPath, imgtype, ssize)) == NULL) {
 		tsk_error_print(stderr);
@@ -186,7 +706,7 @@ main(int argc, char **argv1)
 	}
 
 	string outputFileName = directory_path + "/sparse_image.vhd";
-	int ilen = outputFileName.size();
+	size_t ilen = outputFileName.size();
 	TCHAR *outputFileNameW = (TCHAR *)tsk_malloc((ilen + 1) * sizeof(TCHAR));
 	if (outputFileNameW == NULL) {
 		fprintf(stderr, "tsk_malloc returns NULL\n");
