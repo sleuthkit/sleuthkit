ad183193083098f713306e9698d464e87e0603e9
gaborp <gabor.pilsits@gmail.com>
2019-06-07 16:05:48 +0100

    adding a fix

--- tsk/fs/ntfs_dent.cpp
+++ tsk/fs/ntfs_dent.cpp
@@ -1006,6 +1006,10 @@ ntfs_dir_open_meta(TSK_FS_INFO * a_fs, TSK_FS_DIR ** a_fs_dir,
          * Copy the index allocation run into a big buffer
          */
         idxalloc_len = fs_attr_idx->nrd.allocsize;
+       if (idxalloc_len % 4096 !=0){
+               idxalloc_len = idxalloc_len+ 40; // align the alloc size to the idxrec header to avoid heap overread  by checking the magic header
+       }
+	// 40 the size of the idxrec, otherwise it would allocate 1 byte only 4097-4096
         if ((idxalloc = (char *)tsk_malloc((size_t) idxalloc_len)) == NULL) {
             return TSK_ERR;
         }
