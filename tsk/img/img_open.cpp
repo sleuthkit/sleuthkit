783cb26b28ee11b3b428850909f4b918fab719e3
Joe Ho <jkho@basistech.com>
2019-07-02 12:54:41 -0400

    Update img_open.cpp

--- tsk/img/img_open.cpp
+++ tsk/img/img_open.cpp
@@ -230,6 +230,18 @@ tsk_img_open(int num_img,
         return NULL;
     }
 
+#if HAVE_LIBVHDI
+    case TSK_IMG_TYPE_VHD_VHD:
+        img_info = vhdi_open(num_img, images, a_ssize);
+        break;
+#endif
+
+#if HAVE_LIBVMDK
+    case TSK_IMG_TYPE_VMDK_VMDK:
+        img_info = vmdk_open(num_img, images, a_ssize);
+        break;
+#endif
+
     case TSK_IMG_TYPE_RAW:
         img_info = raw_open(num_img, images, a_ssize);
         break;
