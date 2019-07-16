91dd079bca9f36d9845dd71b5de67c76d71517f2
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2019-07-16 13:45:46 +0100

    * tsk_img_open() now errors out if num_img < 0 * tsk_img_open() now errors out if num_img != 1 for AFF, QCOW, VHD, VMDK.

--- tsk/img/aff.c
+++ tsk/img/aff.c
@@ -222,8 +222,18 @@ aff_close(TSK_IMG_INFO * img_info)
 
 
 TSK_IMG_INFO *
-aff_open(const TSK_TCHAR * const images[], unsigned int a_ssize)
+aff_open(int a_num_img, const TSK_TCHAR * const images[], unsigned int a_ssize)
 {
+    if (a_num_img != 1) {
+        tsk_error_set_errstr("aff_open file: %" PRIttocTSK
+            ": expected one image filename, was given %d", a_images[0], a_num_img);
+
+        if (tsk_verbose != 0) {
+            tsk_fprintf(stderr, "aff requires exactly 1 image filename for opening\n");
+        }
+        return NULL;
+    }
+
     IMG_AFF_INFO *aff_info;
     TSK_IMG_INFO *img_info;
     int type;
