eb9f96b475474a305c73e69626ea8964d618fc74
Eamonn Saunders <esaunders@basistech.com>
2019-08-13 17:22:31 +0000

    Testing OpenJDK build (part 1)

--- travis_build.sh
+++ travis_build.sh
@@ -16,6 +16,10 @@ installLib() {
 if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
 	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev wget
+	sudo apt-get -y install openjdk-8 openjfx=8u161-b12-1ubuntu2 libopenjfx-java=8u161-b12-1ubuntu2 libopenjfx-jni=8u161-b12-1ubuntu2
+	sudo update_alternatives --config java
+	java -version
+	javac -version
 elif test ${TRAVIS_OS_NAME} = "osx"; then
 	export PATH=${PATH}:/usr/local/opt/gettext/bin
 	brew install ant libewf gettext cppunit afflib
