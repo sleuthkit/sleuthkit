9df7be1c5006e82f58b36c40b7291834f81a339f
Eamonn Saunders <esaunders@basistech.com>
2019-08-14 17:41:59 +0000

    This is getting seriously annoying.

--- travis_build.sh
+++ travis_build.sh
@@ -20,7 +20,8 @@ if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -y install openjdk-8-jdk openjfx=8u161-b12-1ubuntu2 libopenjfx-java=8u161-b12-1ubuntu2 libopenjfx-jni=8u161-b12-1ubuntu2
 	sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 	sudo update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
-	echo $JAVA_HOME
+	export PATH=/usr/bin:$PATH
+	unset JAVA_HOME
 	which javac
 
 	java -version
