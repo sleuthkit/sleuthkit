325dec555b6dda73f92b50c1d098fabeaa18b970
Eamonn Saunders <esaunders@basistech.com>
2019-08-14 15:06:18 +0000

    Yet another attempt at getting the build to use the correct version of java.

--- travis_build.sh
+++ travis_build.sh
@@ -19,6 +19,10 @@ if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -y install openjdk-8-jdk openjfx=8u161-b12-1ubuntu2 libopenjfx-java=8u161-b12-1ubuntu2 libopenjfx-jni=8u161-b12-1ubuntu2
 	sudo update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 	sudo update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
+	which javac
+	sudo update-alternatives --config java
+	sudo update-alternatives --config javac
+
 	java -version
 	javac -version
 elif test ${TRAVIS_OS_NAME} = "osx"; then
