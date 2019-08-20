7c3dc272cf1aa57ef9f30f7b9b3289de31c6ccf7
Eamonn Saunders <esaunders@basistech.com>
2019-08-20 16:04:37 +0000

    Trying out a restructuring of our Travis build.

--- travis_build.sh
+++ travis_build.sh
@@ -14,8 +14,8 @@ installLib() {
 }
 
 if test ${TRAVIS_OS_NAME} = "linux"; then
-	sudo apt-get -qq update
-	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev wget
+#	sudo apt-get -qq update
+#	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev wget
 
 	sudo apt-get -y install openjdk-8-jdk openjfx=8u161-b12-1ubuntu2 libopenjfx-java=8u161-b12-1ubuntu2 libopenjfx-jni=8u161-b12-1ubuntu2
 
