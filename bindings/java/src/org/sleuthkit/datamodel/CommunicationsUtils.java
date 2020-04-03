5b0c3efeb18c3d45d135047e6ea9ea39d3e4f16a
Raman Arora <raman@basistech.com>
2020-04-03 09:19:27 -0400

    6219: TskCoreException thrown from normalizePhoneNum.
     Do not attempt to create an account when we do not have a valid phone number or email address.

--- bindings/java/src/org/sleuthkit/datamodel/CommunicationsUtils.java
+++ bindings/java/src/org/sleuthkit/datamodel/CommunicationsUtils.java
@@ -18,6 +18,7 @@
  */
 package org.sleuthkit.datamodel;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
 
 /**
@@ -33,11 +34,10 @@ public final class CommunicationsUtils {
     }
 	
 	/**
-	 * Checks if the given string may be a phone number.
-	 * Normalize the phone number by removing all non numeric characters, except
-	 * for leading +.
+	 * Normalize the given phone number by removing all non numeric characters, 
+	 * except for a leading +.
 	 *
-	 * @param phoneNum The string to check and normalize.
+	 * @param phoneNum The string to normalize.
 	 *
 	 * @return The normalized phone number.
 	 * 
@@ -53,22 +53,69 @@ public final class CommunicationsUtils {
 	}
 
 	/**
-	 * Checks if the given string is a valid email address.
 	 * Normalizes the given email address by converting it to lowercase.
 	 *
-	 * @param emailAddress The string to be checked and normalized.
+	 * @param emailAddress The email address string to be normalized.
 	 *
 	 * @return The normalized email address.
 	 * @throws TskCoreException If the given string is not a valid email address.
 	 */
 	public static String normalizeEmailAddress(String emailAddress) throws TskCoreException {
 		
-		EmailValidator validator = EmailValidator.getInstance(true, true);
-        if (validator.isValid(emailAddress)) {
+        if (isValidEmailAddress(emailAddress)) {
             return emailAddress.toLowerCase();
         } else {
             throw new TskCoreException(String.format("Input string is not a valid email address: %s", emailAddress));
         }
 	}
 	
+	/**
+	 * Checks if the given accountId is a valid id for 
+	 * the specified account type.
+	 * 
+	 * @param accountType Account type.
+	 * @param accountUniqueID Id to check.
+	 * 
+	 * @return True, if the id is a valid id for the given account type, False otherwise.
+	 */
+	public static boolean isValidAccountId(Account.Type accountType, String accountUniqueID) {
+		if (accountType == Account.Type.PHONE) {
+			return isValidPhoneNumber(accountUniqueID);
+		}
+		if (accountType == Account.Type.EMAIL) {
+			return isValidPhoneNumber(accountUniqueID);
+		}
+		
+		return !StringUtils.isEmpty(accountUniqueID);
+	}
+	
+	/**
+	 * Checks if the given string is a valid phone number.
+	 *
+	 * @param phoneNum Phone number string to check.
+	 *
+	 * @return True if the given string is a valid phone number, false otherwise.
+	 */
+	public static boolean isValidPhoneNumber(String phoneNum) {
+		if (!StringUtils.isEmpty(phoneNum)) {
+			return phoneNum.matches("\\+?[0-9()\\-\\s]+");
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks if the given string is a valid email address.
+	 *
+	 * @param emailAddress String to check.
+	 *
+	 * @return True if the given string is a valid email address, false otherwise.
+	 */
+	public static boolean isValidEmailAddress(String emailAddress) {
+		if (!StringUtils.isEmpty(emailAddress)) {
+			EmailValidator validator = EmailValidator.getInstance(true, true);
+			return validator.isValid(emailAddress);
+		}
+
+		return false;
+	}
 }
