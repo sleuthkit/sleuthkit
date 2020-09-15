27f58954db06a9796754ca17324c07204bb43908
apriestman <apriestman@basistech.com>
2020-09-15 14:54:37 -0400

    Changed type for pvol_block and snap_id

--- tools/fstools/fsstat.cpp
+++ tools/fstools/fsstat.cpp
@@ -57,7 +57,7 @@ main(int argc, char **argv1)
     const char * password = "";
 
     TSK_POOL_TYPE_ENUM pooltype = TSK_POOL_TYPE_DETECT;
-    TSK_DADDR_T pvol_block = 0;
+    TSK_OFF_T pvol_block = 0;
 
     int ch;
     uint8_t type = 0;
@@ -210,7 +210,7 @@ main(int argc, char **argv1)
             exit(1);
         }
 
-        img = pool->get_img_info(pool, pvol_block);
+        img = pool->get_img_info(pool, (TSK_DADDR_T)pvol_block);
         if ((fs = tsk_fs_open_img_decrypt(img, imgaddr * img->sector_size, fstype, password)) == NULL) {
             tsk_error_print(stderr);
             if (tsk_error_get_errno() == TSK_ERR_FS_UNSUPTYPE)
