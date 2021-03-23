947902ff4ca2132a3e0801fe9a17e2c758e5a6f8
Raman Arora <raman@basistech.com>
2021-03-23 10:34:40 -0400

    4236: add a deleteAnalysisResult() method overload that takes a transaction.

--- bindings/java/src/org/sleuthkit/datamodel/AnalysisResultsDeletedEvent.java
+++ bindings/java/src/org/sleuthkit/datamodel/AnalysisResultsDeletedEvent.java
@@ -1,15 +1,15 @@
 /*
  * Sleuth Kit Data Model
- * 
+ *
  * Copyright 2021 Basis Technology Corp.
  * Contact: carrier <at> sleuthkit <dot> org
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,18 +18,32 @@
  */
 package org.sleuthkit.datamodel;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Event to indicate that an analysis result was deleted.
+ * Event to indicate that analysis results were deleted.
  */
-public class AnalysisResultDeletedEvent implements TskEvent {
-	
-	private final AnalysisResult deletedResult;
-	
-	AnalysisResultDeletedEvent(AnalysisResult deletedResult) {
-		this.deletedResult = deletedResult;	
+public class AnalysisResultsDeletedEvent implements TskEvent {
+
+	private final List<AnalysisResult> deletedResults;
+
+	/**
+	 * Constructs a new AnalysisResultsDeletedEvent.
+	 *
+	 * @param deletedResults List of deleted results.
+	 */
+	AnalysisResultsDeletedEvent(List<AnalysisResult> deletedResults) {
+		this.deletedResults = deletedResults;
 	}
 
-	public AnalysisResult getDeletedAnalysisResult() {
-		return deletedResult;
+	/**
+	 * Returns a list of deleted results.
+	 *
+	 * @return List of AnalysisResult.
+	 */
+	public List<AnalysisResult> getDeletedAnalysisResult() {
+		return Collections.unmodifiableList(deletedResults);
 	}
+
 }
