c2405957db1f771fbc880f53b7d3577b743d8295
apriestman <apriestman@basistech.com>
2021-05-13 12:55:01 -0400

    Starting on unsupported image type detection

---
+++ tsk/img/unsupported_types.c
@@ -0,0 +1,21 @@
+/*
+** The Sleuth Kit
+**
+** Copyright (c) 2021 Basis Technology Corp.  All rights reserved
+** Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
+**
+** This software is distributed under the Common Public License 1.0
+**
+*/
+
+/**
+ * Check if the given image is a known but unsupported type.
+ *
+ * @return The name of the image type or null if it doesn't match a known type.
+ */
+const char * detectUnsupportedType(TSK_IMG_INFO * img_info) {
+
+
+
+    return NULL;
+}
---
+++ tsk/img/unsupported_types.h
@@ -0,0 +1,20 @@
+/*
+** The Sleuth Kit
+**
+** Copyright (c) 2021 Basis Technology Corp.  All rights reserved
+** Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
+**
+** This software is distributed under the Common Public License 1.0
+**
+*/
+
+#ifndef _UNSUPPORTED_TYPES_H_
+#define _UNSUPPORTED_TYPES_H_
+
+#include "tsk/base/tsk_base.h"
+#include "tsk/img/tsk_img.h"
+#include "tsk/base/tsk_base_i.h"
+
+const char * detectUnsupportedType(TSK_IMG_INFO * img_info);
+
+#endif
