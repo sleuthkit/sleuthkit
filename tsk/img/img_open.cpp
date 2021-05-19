aeabec39251e799d106def37d9948562b63de998
apriestman <apriestman@basistech.com>
2021-05-19 14:30:42 -0400

    Revert unnecessary changes

--- tsk/img/img_open.cpp
+++ tsk/img/img_open.cpp
@@ -17,7 +17,6 @@
 #include "tsk_img_i.h"
 
 #include "raw.h"
-#include "unsupported_types.h"
 
 #if HAVE_LIBAFFLIB
 #include "aff.h"
@@ -276,7 +275,6 @@ tsk_img_open(int num_img,
 
     /* we have a good img_info, set up the cache lock */
     tsk_init_lock(&(img_info->cache_lock));
-
     return img_info;
 }
 
