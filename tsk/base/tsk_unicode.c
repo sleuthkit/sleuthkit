29fbfc43938cc0d229804643532d991f91743d60
Brian Carrier <carrier@sleuthkit.org>
2021-12-06 14:29:29 -0500

    Added endian ordering argument

--- tsk/base/tsk_unicode.c
+++ tsk/base/tsk_unicode.c
@@ -575,23 +575,24 @@ tsk_cleanupUTF8(char *source, const char replacement)
 /**
  * Cleans up the passed in string to replace invalid
  * UTF-16 values with the passed in character.
+ * @param endian Ordering that data is stored in
  * @param source String to be cleaned up
  * @param replacement Character to insert into source as needed.
  */
 void
-tsk_cleanupUTF16_lclorder(wchar_t *source, const wchar_t replacement) {
+tsk_cleanupUTF16(TSK_ENDIAN_ENUM endian, wchar_t *source, const wchar_t replacement) {
 
     size_t total_len = wcslen(source);
     size_t cur_idx = 0;
     
     while (cur_idx < total_len) {
-        UTF32 ch = source[cur_idx];
+        UTF32 ch = tsk_getu16(endian, (uint8_t *) &source[cur_idx]);
 
         /* If we have a surrogate pair, check out the high part. */
         if (ch >= UNI_SUR_HIGH_START && ch <= UNI_SUR_HIGH_END) {
             /* If the 16 bits following the high surrogate are in the source buffer... */
             if (cur_idx + 1 < total_len) {
-                UTF32 ch2 = source[cur_idx+1];
+                UTF32 ch2 = tsk_getu16(endian, (uint8_t *) &source[cur_idx+1]);
                 
                 /* If it's a low surrogate, we're good. */
                 if (ch2 >= UNI_SUR_LOW_START && ch2 <= UNI_SUR_LOW_END) {
