cf817883cb0c73b56ea0787e9f194ce19012272b
Joel Uckelman <joel.uckelman@aon.com>
2024-10-01 17:52:18 +0100

    Run from the correct directory.

--- tests/get_test_images.bash
+++ tests/get_test_images.bash
@@ -2,7 +2,7 @@
 #
 # Install python virtual environment and run get_test_images.py
 #
-cd $(dirname $0)
+pushd tests
 if [ ! -d venv ]; then
     python3 -m venv venv
     source venv/bin/activate
@@ -10,4 +10,5 @@ if [ ! -d venv ]; then
 else
     source venv/bin/activate
 fi
-python3 get_test_images.py
+popd
+python3 tests/get_test_images.py
