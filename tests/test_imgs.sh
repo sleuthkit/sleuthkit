c5b06538e84dd582bef43d47da723e93994c6737
Joel Uckelman <joel.uckelman@aon.com>
2024-10-01 16:52:37 +0100

    Fix path.

--- tests/test_imgs.sh
+++ tests/test_imgs.sh
@@ -2,6 +2,7 @@
 
 ERROR=0
 
+cd tests
 for i in $(cat imgs_to_dump) ; do
   diff data/dump/$i.json <(TZ=America/New_York ./img_dump data/img/$i) && echo "SUCCESS: img_dump $i" || { echo "FAILED: img_dump $i" ; ERROR=1 ; }
 done
