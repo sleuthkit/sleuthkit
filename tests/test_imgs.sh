f7128eff237c8df133ff1241e1d56668215fc956
Joel Uckelman <joel.uckelman@aon.com>
2024-10-01 19:24:21 +0100

    Set up img_dump for wine.

--- tests/test_imgs.sh
+++ tests/test_imgs.sh
@@ -3,8 +3,21 @@
 ERROR=0
 
 cd tests
+
+# find the correct img_dump executable
+if [ -x ./img_dump ]; then
+  IMG_DUMP=./img_dump
+  WINEDEBUG=
+elif [ -x ./img_dump.exe ]; then
+  IMG_DUMP='wine img_dump.exe'
+  WINEDEBUG=-all
+else
+  echo "FAILED: no img_dump executable"
+  exit 1
+fi
+
 for i in $(cat imgs_to_dump) ; do
-  diff data/dump/$i.json <(TZ=America/New_York ./img_dump data/img/$i) && echo "SUCCESS: img_dump $i" || { echo "FAILED: img_dump $i" ; ERROR=1 ; }
+  diff data/dump/$i.json <(TZ=America/New_York $IMG_DUMP data/img/$i) && echo "SUCCESS: img_dump $i" || { echo "FAILED: img_dump $i" ; ERROR=1 ; }
 done
 
 exit $ERROR
