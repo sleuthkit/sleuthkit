7009f5c9f823b55080a02944e86fb92025a401f3
Joel Uckelman <joel.uckelman@aon.com>
2024-10-01 19:51:23 +0100

    Ignore whitespace changes.

--- tests/test_imgs.sh
+++ tests/test_imgs.sh
@@ -17,7 +17,7 @@ else
 fi
 
 for i in $(cat imgs_to_dump) ; do
-  diff data/dump/$i.json <(TZ=America/New_York $IMG_DUMP data/img/$i) && echo "SUCCESS: img_dump $i" || { echo "FAILED: img_dump $i" ; ERROR=1 ; }
+  diff -b data/dump/$i.json <(TZ=America/New_York $IMG_DUMP data/img/$i) && echo "SUCCESS: img_dump $i" || { echo "FAILED: img_dump $i" ; ERROR=1 ; }
 done
 
 exit $ERROR
