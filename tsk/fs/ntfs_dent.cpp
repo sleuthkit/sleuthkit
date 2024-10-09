b43cf5e69ed6915cd31844652e3722884db19a9d
Joel Uckelman <joel.uckelman@aon.com>
2024-10-09 14:25:27 +0100

    Removed superfluous parentheses.

--- tsk/fs/ntfs_dent.cpp
+++ tsk/fs/ntfs_dent.cpp
@@ -1091,7 +1091,7 @@ ntfs_dir_open_meta(TSK_FS_INFO * a_fs, TSK_FS_DIR ** a_fs_dir,
             uint32_t list_len, rec_len;
 
             // Ensure that there is enough data for an idxrec
-            if ((idxalloc_len < sizeof(ntfs_idxrec)) || (off > idxalloc_len - sizeof(ntfs_idxrec))) {
+            if (idxalloc_len < sizeof(ntfs_idxrec) || off > idxalloc_len - sizeof(ntfs_idxrec)) {
                 tsk_error_reset();
                 tsk_error_set_errno(TSK_ERR_FS_INODE_COR);
                 tsk_error_set_errstr
