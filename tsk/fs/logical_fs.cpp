acf29a77b7115ad1e3eadb0debf7793e8783c8ac
Joel Uckelman <joel.uckelman@aon.com>
2024-10-22 14:36:16 +0100

    Fixed string format specifier warning.

--- tsk/fs/logical_fs.cpp
+++ tsk/fs/logical_fs.cpp
@@ -1504,7 +1504,7 @@ logicalfs_read_block(TSK_FS_INFO *a_fs, TSK_FS_FILE *a_fs_file, TSK_DADDR_T a_bl
 		tsk_error_reset();
 		tsk_error_set_errno(TSK_ERR_IMG_READ);
 		tsk_error_set_errstr("logicalfs_read_block: file addr %" PRIuINUM
-			" offset: %" PRIdOFF " read len: %" PRIuSIZE " - %d",
+			" offset: %" PRIdOFF " read len: %u - %d",
 			a_fs_file->meta->addr, a_block_num, block_size,
 			lastError);
 		return -1;
