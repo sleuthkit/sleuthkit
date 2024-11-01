51a2b11179f15490dc8547c246d9b32f551dbffc
Joel Uckelman <joel.uckelman@aon.com>
2024-11-01 19:49:32 +0000

    Mark parameters as unused.

--- tsk/fs/hfs_journal.cpp
+++ tsk/fs/hfs_journal.cpp
@@ -12,7 +12,9 @@
 #include "tsk_hfs.h"
 
 uint8_t
-hfs_jopen(TSK_FS_INFO * fs, TSK_INUM_T inum)
+hfs_jopen(
+  [[maybe_unused]] TSK_FS_INFO * fs,
+  [[maybe_unused]] TSK_INUM_T inum)
 {
     tsk_fprintf(stderr, "jopen not implemented for HFS yet");
 
@@ -20,8 +22,11 @@ hfs_jopen(TSK_FS_INFO * fs, TSK_INUM_T inum)
 }
 
 uint8_t
-hfs_jentry_walk(TSK_FS_INFO * fs, int flags, TSK_FS_JENTRY_WALK_CB action,
-    void *ptr)
+hfs_jentry_walk(
+  [[maybe_unused]] TSK_FS_INFO * fs,
+  [[maybe_unused]] int flags,
+  [[maybe_unused]] TSK_FS_JENTRY_WALK_CB action,
+  [[maybe_unused]] void *ptr)
 {
     tsk_fprintf(stderr, "jentry_walk not implemented for HFS yet");
 
@@ -29,8 +34,13 @@ hfs_jentry_walk(TSK_FS_INFO * fs, int flags, TSK_FS_JENTRY_WALK_CB action,
 }
 
 uint8_t
-hfs_jblk_walk(TSK_FS_INFO * fs, TSK_DADDR_T start, TSK_DADDR_T end,
-    int flags, TSK_FS_JBLK_WALK_CB action, void *ptr)
+hfs_jblk_walk(
+  [[maybe_unused]] TSK_FS_INFO * fs,
+  [[maybe_unused]] TSK_DADDR_T start,
+  [[maybe_unused]] TSK_DADDR_T end,
+  [[maybe_unused]] int flags,
+  [[maybe_unused]] TSK_FS_JBLK_WALK_CB action,
+  [[maybe_unused]] void *ptr)
 {
 
     tsk_fprintf(stderr, "jblk_walk not implemented for HFS yet");
