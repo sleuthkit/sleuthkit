e78908691b2dbc456bb4e25b6134aca93e67a179
Simson Garfinkel <simsong@acm.org>
2024-11-06 16:23:46 -0500

    remove more debug code

--- tools/vstools/mmls.cpp
+++ tools/vstools/mmls.cpp
@@ -238,10 +238,6 @@ mmls_main(int argc, char **argv1)
 
     progname = argv[0];
 
-    fprintf(stderr,"mmls progname=%s\n",progname); // SLG
-    fprintf(stderr,"mmls argv[0]=%s\n",argv[0]);   // SLG
-    fprintf(stderr,"mmls argv[1]=%s\n",argv[1]);   // SLG
-
     while ((ch = GETOPT(argc, argv, _TSK_T("aAb:Bi:mMo:rt:cvVh"))) > 0) {
         switch (ch) {
         case _TSK_T('a'):
