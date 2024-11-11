7d31699a8f6c24e2916a0e8a4af3afca4088a9ed
Joel Uckelman <joel.uckelman@aon.com>
2024-11-11 17:07:49 +0000

    Tests should use tsk_img_close, not tsk_img_free.

--- test/tsk/img/test_raw.cpp
+++ test/tsk/img/test_raw.cpp
@@ -7,9 +7,9 @@
 
 TEST_CASE("raw_open not a file") {
   const TSK_TCHAR* const images[] = { "not_a_file" };
-  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_free)> img{
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
     raw_open(1, images, 1234),
-    tsk_img_free
+    tsk_img_close
   };
   REQUIRE(!img);
 }
@@ -17,18 +17,18 @@ TEST_CASE("raw_open not a file") {
 TEST_CASE("raw_open segment not a file") {
   // test/tsk/img/a_file.1 is a directory
   const TSK_TCHAR* const images[] = { "test/tsk/img/a_file.0" };
-  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_free)> img{
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
     raw_open(1, images, 1234),
-    tsk_img_free
+    tsk_img_close
   };
   REQUIRE(!img);
 }
 
 TEST_CASE("raw_open ok") {
   const TSK_TCHAR* const images[] = { "test/data/image.dd" };
-  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_free)> img{
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
     raw_open(1, images, 0),
-    tsk_img_free
+    tsk_img_close
   };
   REQUIRE(img);
 }
