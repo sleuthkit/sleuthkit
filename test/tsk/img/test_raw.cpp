81f73ed8131b26b8c98c67b14802b37de2db8068
Joel Uckelman <joel.uckelman@aon.com>
2024-11-11 16:16:39 +0000

    Added tests for raw_open().

---
+++ test/tsk/img/test_raw.cpp
@@ -0,0 +1,34 @@
+#include "tsk/img/tsk_img_i.h"
+#include "tsk/img/raw.h"
+
+#include "catch.hpp"
+
+#include <memory>
+
+TEST_CASE("raw_open not a file") {
+  const TSK_TCHAR* const images[] = { "not_a_file" };
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_free)> img{
+    raw_open(1, images, 1234),
+    tsk_img_free
+  };
+  REQUIRE(!img);
+}
+
+TEST_CASE("raw_open segment not a file") {
+  // test/tsk/img/a_file.1 is a directory
+  const TSK_TCHAR* const images[] = { "test/tsk/img/a_file.0" };
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_free)> img{
+    raw_open(1, images, 1234),
+    tsk_img_free
+  };
+  REQUIRE(!img);
+}
+
+TEST_CASE("raw_open ok") {
+  const TSK_TCHAR* const images[] = { "test/data/image.dd" };
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_free)> img{
+    raw_open(1, images, 0),
+    tsk_img_free
+  };
+  REQUIRE(img);
+}
