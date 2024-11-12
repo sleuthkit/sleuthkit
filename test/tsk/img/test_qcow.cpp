b972779bfdb0edea1653bac5e95d259fcaa83288
Joel Uckelman <joel.uckelman@aon.com>
2024-11-12 14:12:28 +0000

    Added tests for qcow_open.

---
+++ test/tsk/img/test_qcow.cpp
@@ -0,0 +1,55 @@
+#include "tsk/img/tsk_img_i.h"
+#include "tsk/img/qcow.h"
+
+#if HAVE_LIBQCOW
+
+#include <memory>
+
+#include "catch.hpp"
+
+TEST_CASE("qcow_open not a file") {
+  const TSK_TCHAR* const images[] = { _TSK_T("not_a_file") };
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
+    qcow_open(1, images, 1234),
+    tsk_img_close
+  };
+  REQUIRE(!img);
+}
+
+TEST_CASE("qcow_open zero images") {
+  const TSK_TCHAR* const images[] = {};
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
+    qcow_open(0, images, 1234),
+    tsk_img_close
+  };
+  REQUIRE(!img);
+}
+
+TEST_CASE("qcow_open two images") {
+  const TSK_TCHAR* const images[] = { _TSK_T("a"), _TSK_T("b") };
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
+    qcow_open(2, images, 1234),
+    tsk_img_close
+  };
+  REQUIRE(!img);
+}
+
+TEST_CASE("qcow_open not a QCOW") {
+  const TSK_TCHAR* const images[] = { _TSK_T("test/data/image.dd") };
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
+    qcow_open(1, images, 1234),
+    tsk_img_close
+  };
+  REQUIRE(!img);
+}
+
+TEST_CASE("qcow_open ok") {
+  const TSK_TCHAR* const images[] = { _TSK_T("test/data/image.qcow") };
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
+    qcow_open(1, images, 1234),
+    tsk_img_close
+  };
+  REQUIRE(img);
+}
+
+#endif
