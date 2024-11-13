dd07e69bab8b7c8476b0c76a8077600a85961cb2
Joel Uckelman <joel.uckelman@aon.com>
2024-11-13 15:36:11 +0000

    Switched some test data paths to test_data/.

--- test/tools/vstools/test_mmls_E01.sh
+++ test/tools/vstools/test_mmls_E01.sh
@@ -2,4 +2,4 @@
 
 TD=test/tools/tool_differ.sh
 
-$TD 'tools/vstools/mmls$EXEEXT -r test/from_brian/exfat1.E01' test/tools/vstools/mmls_output/2
+$TD 'tools/vstools/mmls$EXEEXT -r test_data/exfat1.E01' test/tools/vstools/mmls_output/2
