2495222b775ffeee6bc49512fa32b21294e2730b
Joel Uckelman <joel.uckelman@aon.com>
2024-11-18 13:56:59 +0000

    Put the E01 tests for mmls in the right file.

--- test/tools/vstools/test_mmls.sh
+++ test/tools/vstools/test_mmls.sh
@@ -3,5 +3,3 @@
 TD=test/tools/tool_differ.sh
 
 $TD 'tools/vstools/mmls$EXEEXT -h' test/tools/vstools/mmls_output/1
-$TD 'tools/vstools/mmls$EXEEXT test/from_brian/mbr-disk-image.E01' test/tools/vstools/mmls_output/4
-$TD 'tools/vstools/mmls$EXEEXT test/from_brian/gpt_130_partitions.E01' test/tools/vstools/mmls_output/3
--- test/tools/vstools/test_mmls_E01.sh
+++ test/tools/vstools/test_mmls_E01.sh
@@ -3,3 +3,5 @@
 TD=test/tools/tool_differ.sh
 
 $TD 'tools/vstools/mmls$EXEEXT -r test/from_brian/exfat1.E01' test/tools/vstools/mmls_output/2
+$TD 'tools/vstools/mmls$EXEEXT test/from_brian/gpt_130_partitions.E01' test/tools/vstools/mmls_output/3
+$TD 'tools/vstools/mmls$EXEEXT test/from_brian/mbr-disk-image.E01' test/tools/vstools/mmls_output/4
