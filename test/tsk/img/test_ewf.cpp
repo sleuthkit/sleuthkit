d1d00df8cf8c57a535541942a61a8b122ca7c5c3
Joel Uckelman <joel.uckelman@aon.com>
2024-11-21 15:29:02 +0000

    Added glob_E01 tests with Windows paths containing backslashes.

--- test/tsk/img/test_ewf.cpp
+++ test/tsk/img/test_ewf.cpp
@@ -3,6 +3,7 @@
 
 #if HAVE_LIBEWF
 
+#include <algorithm>
 #include <memory>
 
 #include "catch.hpp"
@@ -45,6 +46,18 @@ TEST_CASE("ewf_open backslash path separator ok") {
 }
 #endif
 
+void check_glob_E01(
+  const TSK_TCHAR* path,
+  bool ok,
+  const std::vector<TSK_TSTRING>& exp)
+{
+  const auto glob = glob_E01(path);
+  CHECK(bool(glob) == ok);
+  if (ok && glob) {
+    CHECK(glob.value() == exp);
+  }
+}
+
 TEST_CASE("glob_E01") {
   const std::tuple<const TSK_TCHAR*, bool, std::vector<TSK_TSTRING>> tcase[] = {
     { _TSK_T("test/data/image.E01"), true, { _TSK_T("test/data/image.E01") } },
@@ -54,12 +67,23 @@ TEST_CASE("glob_E01") {
   };
 
   for (const auto& [path, ok, exp]: tcase) {
-    CAPTURE(path);
-    const auto glob = glob_E01(path);
-    CHECK(bool(glob) == ok);
-    if (ok && glob) {
-      CHECK(glob.value() == exp);
+#ifdef TSK_WIN32
+    // libewf expects paths containing backslashes on Windows
+    TSK_TSTRING p(path);
+    std::replace(p.begin(), p.end(), '/', '\\');
+    const TSK_TCHAR* path_bs = p.c_str();
+
+    std::vector<TSK_TSTRING> exp_bs = exp;
+    for (auto& ep: exp_bs) {
+      std::replace(ep.begin(), ep.end(), '/', '\\');
     }
+
+    CAPTURE(path_bs);
+    check_glob_E01(path_bs, ok, exp_bs);
+#else
+    CAPTURE(path);
+    check_glob_E01(path, ok, exp);
+#endif
   }
 }
 
