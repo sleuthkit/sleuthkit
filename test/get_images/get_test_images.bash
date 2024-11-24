9ecd1ad4fe11d98d39b1e4aba88b0c6a346c96d2
Simson Garfinkel <simsong@acm.org>
2024-11-23 21:26:14 -0500

    fixed path issue

--- test/get_images/get_test_images.bash
+++ test/get_images/get_test_images.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 #
-# Install python virtual environment and run get_test_images.py
+# Install python virtual environment in the same directory as this script.
+# Then go back to where we were and run get_test_images.py
 #
 MYDIR=$(dirname "$0")
 pushd $MYDIR
@@ -12,4 +13,4 @@ else
     source venv/bin/activate
 fi
 popd
-python3 get_test_images.py
+python3 test/get_images/get_test_images.py
