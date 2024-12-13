49140bb3d2928fd8445dc82ad71b7d4f1eaaeaba
Greg DiCristofaro <gregd@basistech.com>
2024-12-13 18:35:54 -0500

    updates

--- test/tsk/hashdb/test_binsrch_index.cpp
+++ test/tsk/hashdb/test_binsrch_index.cpp
@@ -2,6 +2,11 @@
 #include "tsk/hashdb/tsk_hashdb_i.h"
 #include "catch.hpp"
 
+void hdb_binsrch_index_close(TSK_HDB_BINSRCH_INFO *hdb_binsrch_info)
+{
+    hdb_binsrch_close((TSK_HDB_INFO *)hdb_binsrch_info);
+}
+
 void test_hdb_binsrch_idx_init_hash_type_info(
     const TSK_TCHAR *db_name,
     TSK_HDB_HTYPE_ENUM htype,
@@ -10,42 +15,31 @@ void test_hdb_binsrch_idx_init_hash_type_info(
     const TSK_TCHAR *expected_idx_fname,
     const TSK_TCHAR *expected_idx_idx_fname)
 {
-    TSK_HDB_BINSRCH_INFO hdb_binsrch_info;
+    std::unique_ptr<TSK_HDB_BINSRCH_INFO, decltype(&hdb_binsrch_index_close)> hdb_binsrch_info{
+        new TSK_HDB_BINSRCH_INFO{},
+        &hdb_binsrch_index_close};
 
     /**
      * setup values:
-     * normally, this would be done as a part of opening the hashdb, but not 
+     * normally, this would be done as a part of opening the hashdb, but not
      * for this unit test
      **/
-    hdb_binsrch_info.hash_type = TSK_HDB_HTYPE_INVALID_ID;
-    // this value is read only in hdb_binsrch_idx_init_hash_type_info
-    hdb_binsrch_info.base.db_fname = (TSK_TCHAR *)db_name;
-    int ret_val = hdb_binsrch_idx_init_hash_type_info(&hdb_binsrch_info, htype);
+    hdb_binsrch_info->hash_type = TSK_HDB_HTYPE_INVALID_ID;
 
-    REQUIRE(ret_val == expected_return);
+    TSK_TCHAR *db_name_cpy = (TSK_TCHAR *)tsk_malloc((TSTRLEN(db_name) + 1) * sizeof(TSK_TCHAR));
+    TSTRNCPY(db_name_cpy, db_name, TSTRLEN(db_name) + 1);
+    hdb_binsrch_info->base.db_fname = db_name_cpy;
+
+    int ret_val = hdb_binsrch_idx_init_hash_type_info(hdb_binsrch_info.get(), htype);
+
+    CHECK(ret_val == expected_return);
 
     if (expected_return == 0)
     {
         // if successful call, do comparisons
-        REQUIRE(hdb_binsrch_info.hash_len == expected_hash_len);
-        REQUIRE(TSTRCMP(hdb_binsrch_info.idx_fname, expected_idx_fname) == 0);
-        REQUIRE(TSTRCMP(hdb_binsrch_info.idx_idx_fname, expected_idx_idx_fname) == 0);
-    }
-
-    /**
-     * cleanup allocated items from hdb_binsrch_idx_init_hash_type_info.
-     * These are values set by hdb_binsrch_idx_init_hash_type_info during run 
-     * that need to be cleaned up. Normally, they would be cleaned up when the
-     * hash database is closed, but not for this unit test.
-     **/
-    if (hdb_binsrch_info.idx_fname != NULL)
-    {
-        free(hdb_binsrch_info.idx_fname);
-    }
-
-    if (hdb_binsrch_info.idx_idx_fname != NULL)
-    {
-        free(hdb_binsrch_info.idx_idx_fname);
+        CHECK(hdb_binsrch_info->hash_len == expected_hash_len);
+        CHECK(TSTRCMP(hdb_binsrch_info->idx_fname, expected_idx_fname) == 0);
+        CHECK(TSTRCMP(hdb_binsrch_info->idx_idx_fname, expected_idx_idx_fname) == 0);
     }
 }
 
