6be6c89b7b8ff7f541035b2bd6becfdc3574e21d
Joel Uckelman <joel.uckelman@aon.com>
2025-01-15 13:49:59 +0000

    Added multithreaded cache benchmarks for legacy cache, no cache.

--- bench/runner.cpp
+++ bench/runner.cpp
@@ -15,7 +15,10 @@
 
 #include "tsk/libtsk.h"
 
+#include "tsk/img/tsk_img_i.h"
+
 #include <future>
+#include <iostream>
 #include <memory>
 #include <vector>
 
@@ -48,11 +51,12 @@ public:
   }
 };
 
-TEST_CASE("xxx") {
+/*
+TEST_CASE("bench") {
   const TSK_TCHAR* const images[] = { _TSK_T("../fsrip/testdata/img/TinyOSX.E01") };
 
-  BENCHMARK("walk") {
-    std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
+  BENCHMARK("legacy bench") {
+    std::shared_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
       tsk_img_open(1, images, TSK_IMG_TYPE_EWF_EWF, 0),
       tsk_img_close
     };
@@ -82,4 +86,141 @@ TEST_CASE("xxx") {
     }
     return x;
   };
+}
+*/
+
+std::ostream& operator<<(std::ostream& o, const Stats& s) {
+  return o << s.hits << ' '
+           << s.misses << ' '
+           << s.hit_bytes << ' '
+           << s.miss_bytes << ' '
+           << s.hit_ns << ' '
+           << s.miss_ns;
+}
+
+std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> open_img(
+  const std::vector<const TSK_TCHAR*>& images
+)
+{
+  std::unique_ptr<TSK_IMG_INFO, decltype(&tsk_img_close)> img{
+    tsk_img_open(images.size(), images.data(), TSK_IMG_TYPE_DETECT, 0),
+    tsk_img_close
+  };
+  REQUIRE(img);
+  return img;
+}
+
+void do_walk(TSK_IMG_INFO* img) {
+  Walker w;
+  w.openImageHandle(img);
+  if (w.findFilesInImg() != 0) {
+    for (const auto& e: w.getErrorList()) {
+      std::cerr << w.errorRecordToString(e) << std::endl;
+    }
+  }
+}
+
+void do_walks(TSK_IMG_INFO* img, size_t n) {
+  std::vector<std::thread> threads;
+  std::vector<std::future<void>> results;
+
+  for (size_t i = 0; i < n; ++i) {
+/*
+    std::future<void> f = std::async(
+      std::launch::async,
+      [img]() { do_walk(img); }
+    );
+*/
+    std::packaged_task<void(TSK_IMG_INFO*)> task(do_walk);
+    std::future<void> f = task.get_future();
+    threads.emplace_back(std::move(task), img);
+    results.push_back(std::move(f));
+  }
+
+  for (auto& t: threads) {
+    t.join();
+  }
+
+  for (auto& f: results) {
+    f.wait();
+  }
+}
+
+void test_caching(
+  const char* fname,
+  std::shared_ptr<void> (*fsetup)(TSK_IMG_INFO*),
+  const std::vector<const TSK_TCHAR*>& images,
+  size_t threads
+)
+{
+  std::cout << fname << ' ' << threads << ' ';
+  auto img = open_img(images);
+  const auto holder = fsetup(img.get());
+  do_walks(img.get(), threads);
+  std::cout << img->stats << '\n';
+}
+
+TEST_CASE("stats") {
+  const std::tuple<const char*, std::shared_ptr<void> (*)(TSK_IMG_INFO*)> caches[] = {
+    {
+      "tsk_img_read_no_cache",
+      [](TSK_IMG_INFO* img) {
+        img->cache_read = tsk_img_read_no_cache;
+        return std::shared_ptr<void>{nullptr};
+      }
+    },
+    {
+      "tsk_img_read_legacy",
+      [](TSK_IMG_INFO* img) {
+        img->cache_read = tsk_img_read_legacy;
+        return std::shared_ptr<void>{nullptr};
+      }
+    },
+/*
+    {
+      "tsk_img_read_lru",
+      [](TSK_IMG_INFO* img) {
+        img->cache_read = tsk_img_read_lru;
+        img->cache_create = lru_cache_create;
+        img->cache_clone = lru_cache_clone;
+        img->cache_clear = lru_cache_clear;
+        img->cache_free = [](TSK_IMG_INFO*) {};
+
+        std::shared_ptr<Cache> cache{new LRUImgCache(1024)};
+        img->cache_holder = cache.get();
+        return std::static_pointer_cast<void>(cache);
+      }
+    },
+    {
+      "tsk_img_read_lru_tsk_lock",
+      [](TSK_IMG_INFO* img) {
+        img->cache_read = tsk_img_read_lru_own_lock;
+        img->cache_create = lru_cache_create;
+        img->cache_clone = lru_cache_clone;
+        img->cache_clear = lru_cache_clear;
+        img->cache_free = [](TSK_IMG_INFO*) {};
+//        img->cache_free = lru_cache_free;
+
+        std::shared_ptr<Cache> cache{new LRUImgCacheLockingTsk(1024)};
+        img->cache_holder = cache.get();
+        return std::static_pointer_cast<void>(cache);
+      }
+    }
+*/
+  };
+
+  std::vector<std::vector<const TSK_TCHAR*>> images{
+//     { _TSK_T("../fsrip/testdata/img/TinyOSX.E01") },
+     { _TSK_T("/home/juckelman/Downloads/win7-64-nfury-c-drive.E01") }
+  };
+
+  std::cout << "name threads h m \"h bytes\" \"m bytes\" \"h ns\" \"m ns\"\n";
+
+  for (const auto& imgs: images) {
+    for (const auto threads: { 1, 10 }) {
+      for (const auto& [fname, fsetup]: caches) {
+        test_caching(fname, fsetup, imgs, threads);
+      }
+    }
+  }
 }
