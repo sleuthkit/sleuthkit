abe0e1964af5ab68508e439ac4ef3e77aa1df8b7
Simson Garfinkel <simsong@acm.org>
2025-04-01 16:11:29 -0400

    added test_loaddb.sh and two images to test

---
+++ test/tools/autotools/test_loaddb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+"tools/autotools/tsk_loaddb$EXEEXT" -d apfs_pool $SLEUTHKIT_TEST_DATA_DIR/apfs/apfs_pool.E01
+"tools/autotools/tsk_loaddb$EXEEXT" -d issue_3235 $SLEUTHKIT_TEST_DATA_DIR/fuzzing/lvm_test_issue_3235.E01
