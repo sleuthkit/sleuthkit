2795cc3a7bfb324ee091b8d2e1ed64b0a51ae592
Brian Carrier <carrier@sleuthkit.org>
2026-04-26 18:02:39 -0400

    Added error message - from code rabbit

--- tsk/fs/ntfs.c
+++ tsk/fs/ntfs.c
@@ -3309,6 +3309,9 @@ ntfs_load_bmap(NTFS_INFO * ntfs)
     }
     attr_len = tsk_getu32(fs->endian, data_attr->len);
     if ((uintptr_t) data_attr + attr_len > (uintptr_t) mft + ntfs->mft_rsize_b) {
+        tsk_error_reset();
+        tsk_error_set_errno(TSK_ERR_FS_INODE_COR);
+        tsk_error_set_errstr("Error Finding Bitmap Data Attribute: attr_len extends past end of MFT record");
         goto on_error;
     }
 
