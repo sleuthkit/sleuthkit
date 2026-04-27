92b25209728aa0c03ccc3454bdb2d8a0ff27e048
Brian Carrier <carrier@sleuthkit.org>
2026-04-27 13:57:31 -0400

    Added verbose statement for FAT failing 8.3 name check

--- tsk/fs/fatxxfs_meta.c
+++ tsk/fs/fatxxfs_meta.c
@@ -293,8 +293,14 @@ fatxxfs_is_dentry(FATFS_INFO *a_fatfs, FATFS_DENTRY *a_dentry, FATFS_DATA_UNIT_A
         }
 		
 		else if((a_fatfs->subtype == TSK_FATFS_SUBTYPE_SPEC) &&
-                (is_83_name(dentry, strict) == 0))
-			return 0;
+                (is_83_name(dentry, strict) == 0)) {
+            if (!strict) {
+                tsk_fprintf(stderr,
+                    "%s: skipping entry in allocated directory with invalid 8.3 name\n",
+                    func_name);
+            }
+            return 0;
+        }
 
         // basic sanity check on values
         else if ((tsk_getu16(fs->endian, dentry->ctime) == 0)
