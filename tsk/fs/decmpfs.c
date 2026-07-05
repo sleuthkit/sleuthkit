ceb4ce852ad6516217782f071587e70d5d7b6da3
André Berenguel <andre.berenguel@gmail.com>
2026-07-05 12:57:34 -0300

    apfs: prevent integer underflow reading corrupted APFS images

--- tsk/fs/decmpfs.c
+++ tsk/fs/decmpfs.c
@@ -303,6 +303,11 @@ decmpfs_read_lzvn_block_table(const TSK_FS_ATTR *rAttr, CMP_OFFSET_ENTRY** offse
     }
 
     tableDataSize = tsk_getu32(TSK_LIT_ENDIAN, fourBytes);
+    if (tableDataSize < 4) {
+        error_returned
+            (" %s: tableDataSize %u is too small", __func__, tableDataSize);
+        return 0;
+    }
 
     offsetTableData = tsk_malloc(tableDataSize);
     if (offsetTableData == NULL) {
